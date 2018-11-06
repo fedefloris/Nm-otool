4778ee2020fa8a2930ba8b3b16842b9da13a0814
Dav Højt <davhojt@gmail.com>
2018-11-06 08:04:39 +0100

    feature/added placeholder files.

---
+++ nm/srcs/nm.c
@@ -0,0 +1,9 @@
+#include "nm.h"
+
+int					main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	ft_putendl("nm: nothing here");
+	return (0);
+}
---
+++ otool/srcs/otool.c
@@ -0,0 +1,9 @@
+#include "otool.h"
+
+int					main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	ft_putendl("otool: nothing here");
+	return (0);
+}
