7cd501d0d16dbc110013e3c9b548b5d0142e1d85
kernel_j <jenniferwong89@gmail.com>
2018-11-08 10:46:58 +0100

    add check for 32 or 64 bit arch

--- otool/srcs/main/otool.c
+++ otool/srcs/main/otool.c
@@ -1,6 +1,20 @@
 #include <sys/stat.h>
 #include <sys/mman.h>
 #include "otool.h"
+#include "nm_otool.h"
+#include "ft_printf.h"
+
+int					otool(t_file *file)
+{
+    uint32_t    magic_num;
+
+    magic_num = *(uint32_t *)file->map;
+    if (magic_num == MH_MAGIC_64)
+        ft_printf("64 bit obj\n");
+    else if (magic_num == MH_MAGIC)
+        ft_printf("32 bit obj\n");
+    return (SUCCESS);
+}
 
 int					main(int argc, char **argv)
 {
@@ -14,6 +28,8 @@ int					main(int argc, char **argv)
         ft_bzero(&file, sizeof(t_file));
         if ((ret = load_file(argv[i], &file) == SUCCESS))
         {
+            ft_printf("return status of otool: %d\n", otool(&file));
+            munmap(file.map, file.file_size);
         }
     }
     return (SUCCESS);
