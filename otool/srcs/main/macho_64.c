5f6a2e679f5a263208b42b3b9c9be5e4b74e974c
Jennifer Wong <jenniferwong89@gmail.com>
2018-11-10 17:17:54 +0100

    modify text_segment to text_segment_64; add condition to find __TEXT, _text

--- otool/srcs/main/macho_64.c
+++ otool/srcs/main/macho_64.c
@@ -2,17 +2,25 @@
 #include "otool.h"
 #include "ft_printf.h"
 
-int     text_segment(struct load_command *lcmd)
+int     text_segment_64(struct load_command *lcmd, t_file *file)
 {
     struct segment_command_64   *segment;
+	struct section_64			*section;
+	void						*sect_ptr;
     uint32_t                    i;
 
     segment = (struct segment_command_64 *)lcmd;
+	sect_ptr = (void *)(segment + 1);
     i = 0;
     while (i < segment->nsects)
     {
-        ft_printf("%s\n", segment->segname);
-        segment = (void *)segment + segment->cmdsize;
+		section = (struct section_64 *)sect_ptr;
+		if (ft_strcmp(segment->segname, SEG_TEXT) == 0
+				&& ft_strcmp(section->sectname, SECT_TEXT) == 0)
+		{
+			ft_printf("segment: %s , section: %s\n", segment->segname, section->sectname);
+		}
+		sect_ptr += sizeof(struct section_64);
         i++;
     }
     return (SUCCESS);
@@ -25,13 +33,14 @@ int     macho_64(t_file *file)
     uint32_t                    i;
 
     header = (struct mach_header_64 *)file->map;
+	filetype_64(header);
     lcmd = (void *)file->map + sizeof(*header);
     i = 0;
     while (i < header->ncmds)
     {
         if (lcmd->cmd == LC_SEGMENT_64)
         {
-            text_segment(lcmd);
+            text_segment_64(lcmd, file);
         }
         lcmd = (void *)lcmd + lcmd->cmdsize;
         i++;
