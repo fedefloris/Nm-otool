70012752130600d787b43972bb5772659460a678
Jennifer Wong <jenniferwong89@gmail.com>
2018-11-10 17:13:04 +0100

    add check_file_stat

--- otool/srcs/main/load_file.c
+++ otool/srcs/main/load_file.c
@@ -2,7 +2,17 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <sys/mman.h>
+#include "ft_printf.h"
 #include "otool.h"
+
+int	check_file_stat(char *filename, struct stat *buf)
+{
+	if (buf->st_mode & S_IFREG)
+		ft_printf("regular file\n");
+	else if (buf->st_mode & S_IFDIR)
+		ft_printf("'%s': Is a directory\n", filename);
+	return (SUCCESS);
+}
 
 int load_file(char *filename, t_file *file)
 {
@@ -18,6 +28,7 @@ int load_file(char *filename, t_file *file)
         close(fd);
         return (FAILURE);
     }
+	check_file_stat(filename, &buf);
     if ((file->map = mmap(0, buf.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) == MAP_FAILED)
     {
         close(fd);
