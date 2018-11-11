e0c323c9e806ebb0c84eb6bbc8b92a1837b33718
Dav Højt <davhojt@gmail.com>
2018-11-11 22:27:25 +0100

    Added struct for nm

--- nm/includes/nm.h
+++ nm/includes/nm.h
@@ -5,6 +5,13 @@
 # include "ft_printf.h"
 # include <stdlib.h>
 
+typedef struct	s_nm
+{
+	char		**argv;
+
+	struct stat	stat;
+}				t_nm;
+
 int     list_object_files_symbols(int argc, char **argv);
 int     list_object_file_symbols(char *file_name);
 #endif
