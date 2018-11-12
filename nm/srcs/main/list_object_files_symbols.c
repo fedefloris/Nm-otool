8bc1b0adfbc432e2d71c01c808d9596921fc372f
Dav Højt <davhojt@gmail.com>
2018-11-12 01:22:31 +0100

    refactored argv

--- nm/srcs/main/list_object_files_symbols.c
+++ nm/srcs/main/list_object_files_symbols.c
@@ -3,17 +3,14 @@
 int 		list_object_files_symbols(t_nm *nm)
 {
 	int	exit_status;
-	int i;
 
 	exit_status = EXIT_SUCCESS;
 	if (nm->argc == 1)
 		return (list_object_file_symbols(nm, "a.out"));
-	i = 1;
-	while (i < nm->argc)
+	while (*(++nm->argv))
 	{
-		if (list_object_file_symbols(nm, nm->argv[i]) == EXIT_FAILURE)
+		if (list_object_file_symbols(nm, *nm->argv) == EXIT_FAILURE)
 			exit_status = EXIT_FAILURE;
-		i++;
 	}
 	return (exit_status);
 }
