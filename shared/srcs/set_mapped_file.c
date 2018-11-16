6f5df0a160610db58487f8bc558260bdb9e78bd8
Dav Højt <davhojt@gmail.com>
2018-11-16 12:25:24 +0000

    refactored for new structure.

--- shared/srcs/set_mapped_file.c
+++ shared/srcs/set_mapped_file.c
@@ -3,41 +3,41 @@
 #include <unistd.h>
 #include <sys/mman.h>
 
-int					open_file(t_file *file, char **env)
+int					open_file(t_nm_otool *nm_otool)
 {
 	int				fd;
 	char			*binary_path;
 
-	if ((fd = open(file->name, O_RDONLY)) >= 0)
+	if ((fd = open(nm_otool->file.name, O_RDONLY)) >= 0)
 		return (fd);
-	if (!(binary_path = find_binary(file, env)))
+	if (!(binary_path = find_binary(nm_otool->env)))
 		return (-1);
 	fd = open(binary_path, O_RDONLY);
 	free(binary_path);
 	return (fd);
 }
 
-bool      			set_mapped_file(t_file *file, char *file_name, char **env)
+bool					set_mapped_file(t_nm_otool *nm_otool)
 {
 	bool				status;
-	struct stat	stat;
+	struct stat			stat;
 	int					fd;
 
 	status = true;
-	if ((fd = open_file(file, env) < 0))
+	if ((fd = open_file(file) < 0))
 		status = false;
 	if (status && fstat(fd, &stat) < 0)
 		status = false;
-	file->size = stat.st_size;
+	nm_otool->file.size = stat.st_size;
 	if (status)
-		file->memory = mmap(0, stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-	if (status && file->memory == MAP_FAILED)
+		nm_otool->file.memory = mmap(0, stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+	if (status && nm_otool->file.memory == MAP_FAILED)
 		status = false;
 	if (status && !S_ISREG(stat.st_mode))
 		status = false;
 	if (fd != -1)
 		close(fd);
-	if (status && set_file_format(file) == false)
+	if (status && set_file_format(nm_otool) == false)
 		status = false;
 	return (status);
 }
