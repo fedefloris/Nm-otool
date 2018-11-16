dcf95909560e54252da0d8a518a7d84b80e9b187
Dav Højt <davhojt@gmail.com>
2018-11-16 11:59:35 +0000

    refactored for new structure.

--- shared/srcs/list_object_file_symbols.c
+++ shared/srcs/list_object_file_symbols.c
@@ -1,13 +1,13 @@
 #include "nm_otool.h"
 #include <stdlib.h>
 
-int					list_object_file_symbols(char *file_name, char **env)
+int					list_object_file_symbols(t_nm_otool *nm_otool, char *file_name)
 {
-	t_file		file;
-
-	if (!set_mapped_file(&file, file_name, env))
+	ft_bzero(nm_otool->file, sizeof(*nm_otool->file));
+	nm_otool->file.name = file_name;
+	if (!set_mapped_file(nm_otool))
 		return (EXIT_FAILURE);
-	if (!unset_mapped_file(&file))
+	if (!unset_mapped_file(nm_otool))
 		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
