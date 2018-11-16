ab03fe979965d714da40aeb9a04350bd8ea1126e
fedefloris <fzfloris@gmail.com>
2018-11-16 14:56:03 +0100

    Fix bad file descriptor

--- shared/srcs/set_mapped_file.c
+++ shared/srcs/set_mapped_file.c
@@ -5,10 +5,10 @@
 
 int					open_file(t_file *file, char **env)
 {
-	int				fd;
 	char			*binary_path;
+	int				fd;
 
-	if ((fd = open(file->name, O_RDONLY)) >= 0)
+	if ((fd = open(file->name, O_RDONLY)) > 0)
 		return (fd);
 	if (!(binary_path = find_binary(file, env)))
 		return (-1);
@@ -16,7 +16,6 @@ int					open_file(t_file *file, char **env)
 	free(binary_path);
 	return (fd);
 }
-
 bool      			set_mapped_file(t_file *file, char *file_name, char **env)
 {
 	bool				status;
@@ -25,13 +24,13 @@ bool      			set_mapped_file(t_file *file, char *file_name, char **env)
 
 	status = true;
 	file->name = file_name;
-	if ((fd = open_file(file, env) < 0))
+	if ((fd = open_file(file, env)) < 0)
 		status = false;
 	if (status && fstat(fd, &stat) < 0)
 		status = false;
 	file->size = stat.st_size;
 	if (status)
-		file->memory = mmap(0, stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+		file->memory = mmap(0, file->size, PROT_READ, MAP_PRIVATE, fd, 0);
 	if (status && file->memory == MAP_FAILED)
 		status = false;
 	if (status && !S_ISREG(stat.st_mode))
