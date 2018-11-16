dbb6dcc7a576195529fe9f0f55084255db33a6e8
Dav Højt <davhojt@gmail.com>
2018-11-16 12:22:17 +0000

    refactored for new structure.

--- shared/srcs/set_file_format.c
+++ shared/srcs/set_file_format.c
@@ -2,21 +2,21 @@
 #include "ft_printf.h"
 #include <stdint.h>
 
-bool				set_file_format(t_file *file)
+bool				set_file_format(t_nm_otool *nm_otool)
 {
 #ifdef __APPLE__
 	uint32_t		magic_number;
 
-	magic_number = *(uint32_t *)file->memory;
+	magic_number = *(uint32_t *)nm_otool->file.->memory;
 	if (magic_number == MH_MAGIC)
-		file->format = BITS_32;
+		nm_otool->file.->format = BITS_32;
 	else if (magic_number == MH_MAGIC_64)
-		file->format = BITS_64;
+		nm_otool->file.->format = BITS_64;
 	else
 		return (false);
 	return (true);
 #else
-	(void)file;
+	(void)nm_otool->file.;
 	return (false);
 #endif
 }
