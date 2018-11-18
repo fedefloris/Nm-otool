ca135f1d911f0ec05a84b76c548c3b50cb59633f
fedefloris <fzfloris@gmail.com>
2018-11-18 17:02:24 +0100

    Code refactoring of set_file (added also error logs)

--- shared/srcs/set_file.c
+++ shared/srcs/set_file.c
@@ -1,9 +1,44 @@
 #include "nm_otool.h"
+#include "ft_printf.h"
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/mman.h>
 
-static int		open_file(t_nm_otool *nm_otool)
+static bool	is_a_regular_file(t_nm_otool *nm_otool, int fd)
+{
+	if (!S_ISREG(nm_otool->file.mode))
+	{
+		close(fd);
+		return (false);
+	}
+	return (true);
+}
+
+static bool	map_file_to_memory(t_nm_otool *nm_otool, int fd)
+{
+	nm_otool->file.memory = mmap(0, nm_otool->file.size,
+		PROT_READ, MAP_PRIVATE, fd, 0);
+	close(fd);
+	if (nm_otool->file.memory == MAP_FAILED)
+		return (false);
+	return (true);
+}
+
+static bool	set_file_data(t_nm_otool *nm_otool, int fd)
+{
+	struct stat	stat;
+
+	if (fstat(fd, &stat) < 0)
+	{
+		close(fd);
+		return (false);
+	}
+	nm_otool->file.size = stat.st_size;
+	nm_otool->file.mode = stat.st_mode;
+	return (true);
+}
+
+static int	open_file(t_nm_otool *nm_otool)
 {
 	char			*binary_path;
 	int				fd;
@@ -17,27 +52,21 @@ static int		open_file(t_nm_otool *nm_otool)
 	return (fd);
 }
 
-bool					set_file(t_nm_otool *nm_otool)
+bool				set_file(t_nm_otool *nm_otool)
 {
-	bool				status;
-	struct stat			stat;
-	int					fd;
+	int		fd;
 
-	status = true;
 	if ((fd = open_file(nm_otool)) < 0)
-		status = false;
-	if (status && fstat(fd, &stat) < 0)
-		status = false;
-	nm_otool->file.size = stat.st_size;
-	if (status)
-		nm_otool->file.memory = mmap(0, stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-	if (status && nm_otool->file.memory == MAP_FAILED)
-		status = false;
-	if (status && !S_ISREG(stat.st_mode))
-		status = false;
-	if (fd != -1)
-		close(fd);
-	if (status && !set_file_info(nm_otool))
-		status = false;
-	return (status);
+		ft_printf("%s file not found\n", ERROR_HEADER);
+	else if (!set_file_data(nm_otool, fd))
+		ft_printf("%s fstat call failed\n", ERROR_HEADER);
+	else if (!is_a_regular_file(nm_otool, fd))
+		ft_printf("%s not a regular file\n", ERROR_HEADER);
+	else if (!map_file_to_memory(nm_otool, fd))
+		ft_printf("%s mmap call failed\n", ERROR_HEADER);
+	else if (!set_file_info(nm_otool))
+		;
+	else
+		return (true);
+	return (false);
 }
