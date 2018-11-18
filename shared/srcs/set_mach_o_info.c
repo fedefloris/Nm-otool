56b0d2d2b1d61d8ba7d8e81f9a7ebe087d155b05
fedefloris <fzfloris@gmail.com>
2018-11-18 13:35:55 +0100

    Fix segfault with Mach-o magic number (didn't check if it compiles with MacOS)

--- shared/srcs/set_mach_o_info.c
+++ shared/srcs/set_mach_o_info.c
@@ -2,16 +2,47 @@
 #include <mach-o/loader.h>
 #include <stdint.h>
 
-bool			set_mach_o_info(t_nm_otool *nm_otool)
+static void	set_64_format(t_nm_otool *nm_otool, uint32_t magic_number)
+{
+	nm_otool->file.format = MACH_O_64_FORMAT;
+	nm_otool->file.endianness = LITTLE_ENDIAN_TYPE;
+	if (magic_number == MH_CIGAM_64)
+		nm_otool->file.endianness = BIG_ENDIAN_TYPE;
+}
+
+static void	set_32_format(t_nm_otool *nm_otool, uint32_t magic_number)
+{
+	nm_otool->file.format = MACH_O_32_FORMAT;
+	nm_otool->file.endianness = LITTLE_ENDIAN_TYPE;
+	if (magic_number == MH_CIGAM)
+		nm_otool->file.endianness = BIG_ENDIAN_TYPE;
+}
+
+static bool	set_format(t_nm_otool *nm_otool)
 {
 	uint32_t		magic_number;
 
 	magic_number = *(uint32_t *)nm_otool->file.memory;
-	if (magic_number == MH_MAGIC)
-		nm_otool->file.format = MACH_O_32_FORMAT;
-	else if (magic_number == MH_MAGIC_64)
-		nm_otool->file.format = MACH_O_64_FORMAT;
+	if (magic_number == MH_MAGIC || magic_number == MH_CIGAM)
+		set_32_format(nm_otool, magic_number);
+	else if (magic_number == MH_MAGIC_64 || magic_number == MH_CIGAM_64)
+	{
+		set_64_format(nm_otool, magic_number);
+		if (nm_otool->file.size < (long)sizeof(struct mach_header_64))
+			return (false);
+	}
 	else
 		return (false);
 	return (true);
+}
+
+bool				set_mach_o_info(t_nm_otool *nm_otool)
+{
+	if (nm_otool->file.size < (long)sizeof(struct mach_header))
+		ft_printf("%s Bad size\n", ERROR_HEADER);
+	else if(!set_format(nm_otool))
+		ft_printf("%s Bad magic number\n", ERROR_HEADER);
+	else
+		return (true);
+	return (false);
 }
