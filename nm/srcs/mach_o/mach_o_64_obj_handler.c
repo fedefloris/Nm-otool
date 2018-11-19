53ba3562d919f52bffcc1c6dcdde2911f4c9d059
Dav Højt <davhojt@gmail.com>
2018-11-19 02:09:01 +0100

    now prints in the same way as the video example.

--- nm/srcs/mach_o/mach_o_64_obj_handler.c
+++ nm/srcs/mach_o/mach_o_64_obj_handler.c
@@ -1,5 +1,22 @@
 #include "nm_otool.h"
 #include "nm.h"
+#include <mach-o/nlist.h>
+
+static void			print_nm(t_nm_otool *nm_otool, int nsyms, int symoff,  int stroff)
+{
+	int				i;
+	char			*stringtable;
+	struct nlist_64	*array;
+
+	array = nm_otool->file.memory + symoff;
+	stringtable = nm_otool->file.memory + stroff;
+	i = 0;
+	while (i < nsyms)
+	{
+		ft_printf("%s\n",stringtable + array[i].n_un.n_strx);
+		i++;
+	}
+}
 
 bool				mach_o_64_obj_handler(t_nm_otool *nm_otool)
 {
@@ -7,6 +24,7 @@ bool				mach_o_64_obj_handler(t_nm_otool *nm_otool)
 	int						i;
 	struct mach_header_64	*header;
 	struct load_command		*lc;
+	struct symtab_command	*sym;
 
 	header = (struct mach_header_64 *)nm_otool->file.memory;
 	ncmds = header->ncmds;
@@ -16,11 +34,12 @@ bool				mach_o_64_obj_handler(t_nm_otool *nm_otool)
 	{
 		if (lc->cmd == LC_SYMTAB)
 		{
+			sym = (struct symtab_command *) lc;
+			print_nm(nm_otool, sym->nsyms, sym->symoff, sym->stroff);
 			break ;
 		}
+		i++;
 		lc = (void *)lc + lc->cmdsize;
 	}
-
-	ft_printf("Mach-o-64, File_name: %s\n", nm_otool->file.name);
 	return (true);
 }
