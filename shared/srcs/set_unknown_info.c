131afc9878b9d722332d92a41189cdd9d7b9609c
Federico <fzfloris@gmail.com>
2018-11-19 18:09:04 +0100

    Add set_unknown_info for not suported OS

---
+++ shared/srcs/set_unknown_info.c
@@ -0,0 +1,9 @@
+#include "nm_otool.h"
+#include "ft_printf.h"
+
+bool			set_unknown_info(t_nm_otool *nm_otool)
+{
+	(void)nm_otool;
+	ft_printf("%s OS not supported\n", ERROR_HEADER);
+	return (false);
+}
