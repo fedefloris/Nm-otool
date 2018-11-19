a359cd73b8b24bab5a4c14ac83e7b92b5b87d59e
fedefloris <fzfloris@gmail.com>
2018-11-19 20:42:05 +0100

    Add config_file inside list_obj_symbols.c

--- shared/srcs/list_obj_symbols.c
+++ shared/srcs/list_obj_symbols.c
@@ -1,10 +1,15 @@
 #include "nm_otool.h"
 
-int			list_obj_symbols(t_nm_otool *nm_otool, char *file_name,
-	t_obj_handler obj_handler)
+static void	config_file(t_nm_otool *nm_otool, char *file_name)
 {
 	ft_bzero(&nm_otool->file, sizeof(nm_otool->file));
 	nm_otool->file.name = file_name;
+}
+
+int					list_obj_symbols(t_nm_otool *nm_otool, char *file_name,
+	t_obj_handler obj_handler)
+{
+	config_file(nm_otool, file_name);
 	if (!set_file(nm_otool))
 		return (EXIT_FAILURE);
 	if (obj_handler && !obj_handler(nm_otool))
