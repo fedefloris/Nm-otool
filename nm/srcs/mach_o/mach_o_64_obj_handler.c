7422958299179cd88624be16a130946b22260a2b
Dav Højt <davhojt@gmail.com>
2018-11-20 00:30:09 +0100

    refactor

--- nm/srcs/mach_o/mach_o_64_obj_handler.c
+++ nm/srcs/mach_o/mach_o_64_obj_handler.c
@@ -1,16 +1,16 @@
 #include "nm_otool.h"
 #include "nm.h"
 
-static void			print_nm(t_nm_otool *nm_otool, int nsyms, int symoff,  int stroff)
+static void			print_nm(t_nm_otool *nm_otool, struct symtab_command *symtab)
 {
-	int				i;
+	uint32_t		i;
 	char			*stringtable;
 	struct nlist_64	*array;
 
-	array = nm_otool->file.memory + symoff;
-	stringtable = nm_otool->file.memory + stroff;
+	array = nm_otool->file.memory + symtab->symoff;
+	stringtable = nm_otool->file.memory + symtab->stroff;
 	i = 0;
-	while (i < nsyms)
+	while (i < symtab->nsyms)
 	{
 		ft_printf("%s\n",stringtable + array[i].n_un.n_strx);
 		i++;
@@ -19,24 +19,23 @@ static void			print_nm(t_nm_otool *nm_otool, int nsyms, int symoff,  int stroff)
 
 bool				mach_o_64_obj_handler(t_nm_otool *nm_otool)
 {
-	int						ncmds;
+	int						number_of_commands;
 	struct mach_header_64	*header;
-	struct load_command		*lc;
-	struct symtab_command	*sym;
+	struct load_command		*load_command;
+	struct symtab_command	*symtab;
 
 	header = (struct mach_header_64 *)nm_otool->file.memory;
 	number_of_commands = header->ncmds;
-	lc = (void *)nm_otool->file.memory + sizeof(*header);
+	load_command = nm_otool->file.memory + sizeof(*header);
 	while (number_of_commands--)
 	{
-		if (lc->cmd == LC_SYMTAB)
+		if (load_command->cmd == LC_SYMTAB)
 		{
-			sym = (struct symtab_command *) lc;
-			print_nm(nm_otool, sym->nsyms, sym->symoff, sym->stroff);
+			symtab = (struct symtab_command *) load_command;
+			print_nm(nm_otool, symtab);
 			break ;
 		}
-		i++;
-		lc = (void *)lc + lc->cmdsize;
+		load_command = (void *)load_command + load_command->cmdsize;
 	}
 	return (true);
 }
