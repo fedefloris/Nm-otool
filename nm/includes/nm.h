75c867a4819bd2bccabd541c2d483a3c592b8dd2
Federico <fzfloris@gmail.com>
2018-11-19 18:36:04 +0100

    Add OBJ_HANDLER macro

--- nm/includes/nm.h
+++ nm/includes/nm.h
@@ -3,14 +3,38 @@
 
 # define DEFAULT_ARGUMENT	"a.out"
 
+# ifdef __APPLE__
+
+# define OBJ_HANDLER(x) mach_o_obj_handler(x)
+
+# elif __linux__
+
+# define OBJ_HANDLER(x) elf_obj_handler(x)
+
+# else
+
+# define OBJ_HANDLER(x) unknown_obj_handler(x)
+
+# endif
+
 bool			obj_handler(t_nm_otool *nm_otool);
 
-#ifdef __APPLE__
+# ifdef __APPLE__
+
+bool			mach_o_obj_handler(t_nm_otool *nm_otool);
 bool			mach_o_32_obj_handler(t_nm_otool *nm_otool);
 bool			mach_o_64_obj_handler(t_nm_otool *nm_otool);
-#elif __linux__
+
+# elif __linux__
+
+bool			elf_obj_handler(t_nm_otool *nm_otool);
 bool			elf_32_obj_handler(t_nm_otool *nm_otool);
 bool			elf_64_obj_handler(t_nm_otool *nm_otool);
-#endif
+
+# else
+
+bool			unknown_obj_handler(t_nm_otool *nm_otool);
+
+# endif
 
 #endif
--- nm/srcs/main/obj_handler.c
+++ nm/srcs/main/obj_handler.c
@@ -3,16 +3,5 @@
 
 bool		obj_handler(t_nm_otool *nm_otool)
 {
-#ifdef __APPLE__
-	if (nm_otool->file.format == MACH_O_32_FORMAT)
-		return (mach_o_32_obj_handler(nm_otool));
-	else if (nm_otool->file.format == MACH_O_64_FORMAT)
-		return (mach_o_64_obj_handler(nm_otool));
-#elif __linux__
-	if (nm_otool->file.format == ELF_32_FORMAT)
-		return (elf_32_obj_handler(nm_otool));
-	else if (nm_otool->file.format == ELF_64_FORMAT)
-		return (elf_32_obj_handler(nm_otool));
-#endif
-	return (false);
+	return (OBJ_HANDLER(nm_otool));
 }
