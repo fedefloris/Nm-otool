f6f0038a7232bd225573482bf9fe8b943e5a1a4d
Dav Højt <davhojt@gmail.com>
2018-11-19 01:00:49 +0100

    Added basic beginning of nm

--- nm/srcs/mach_o/mach_o_64_obj_handler.c
+++ nm/srcs/mach_o/mach_o_64_obj_handler.c
@@ -1,8 +1,26 @@
 #include "nm_otool.h"
 #include "nm.h"
 
-bool		mach_o_64_obj_handler(t_nm_otool *nm_otool)
+bool				mach_o_64_obj_handler(t_nm_otool *nm_otool)
 {
+	int						ncmds;
+	int						i;
+	struct mach_header_64	*header;
+	struct load_command		*lc;
+
+	header = (struct mach_header_64 *)nm_otool->file.memory;
+	ncmds = header->ncmds;
+	i = 0;
+	lc = (void *)nm_otool->file.memory + sizeof(*header);
+	while (i < ncmds)
+	{
+		if (lc->cmd == LC_SYMTAB)
+		{
+			break ;
+		}
+		lc = (void *)lc + lc->cmdsize;
+	}
+
 	ft_printf("Mach-o-64, File_name: %s\n", nm_otool->file.name);
 	return (true);
 }
