a7f4991d662486cb8f8e43d4c85efe740d79b800
Dav Højt <davhojt@gmail.com>
2018-11-20 09:51:37 +0100

    saving tests.

--- shared/srcs/is_safe_address.c
+++ shared/srcs/is_safe_address.c
@@ -2,6 +2,8 @@
 
 bool				is_safe_address(t_nm_otool *nm_otool, void *address)
 {
+	ft_printf("SIZ: %x\nEND: %p\nADD: %p\n",
+		nm_otool->file.size, nm_otool->file.end_of_file, address);
 	if (address > nm_otool->file.end_of_file)
 		return (false);
 	return (true);
