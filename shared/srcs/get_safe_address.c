9747e8220c6d1b7e7ae2620a7472415154b407ec
Dav Højt <davhojt@gmail.com>
2018-11-20 23:42:34 +0100

    refactored.

--- shared/srcs/get_safe_address.c
+++ shared/srcs/get_safe_address.c
@@ -5,6 +5,5 @@ void				*get_safe_address(t_nm_otool *nm_otool, void *address)
 	if (address < nm_otool->file.memory
 		|| address > nm_otool->file.end_of_file)
 		return (NULL);
-	else
-		return (address);
+	return (address);
 }
