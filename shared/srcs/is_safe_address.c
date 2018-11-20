981d3b3be6da403c21854fb97fc398110b8ef9a0
Dav Højt <davhojt@gmail.com>
2018-11-20 21:49:46 +0100

    modified safe address function.

--- shared/srcs/is_safe_address.c
+++ shared/srcs/is_safe_address.c
@@ -1,10 +1,10 @@
 #include "nm_otool.h"
 
-bool				is_safe_address(t_nm_otool *nm_otool, void *address)
+void				*get_safe_address(t_nm_otool *nm_otool, void *address)
 {
-	ft_printf("SIZ: %x\nEND: %p\nADD: %p\n",
-		nm_otool->file.size, nm_otool->file.end_of_file, address);
-	if (address > nm_otool->file.end_of_file)
-		return (false);
-	return (true);
+	if (address < nm_otool->file.memory
+		|| address > nm_otool->file.end_of_file)
+		return (NULL);
+	else
+		return (address);
 }
