f7ce52e2c6b6ab2fda55cdfd7c53eb58f46902f0
Dav Højt <davhojt@gmail.com>
2018-11-21 01:59:18 +0100

    protected assingns in print_nm

--- nm/srcs/mach_o/mach_o_64_obj_handler.c
+++ nm/srcs/mach_o/mach_o_64_obj_handler.c
@@ -1,20 +1,26 @@
 #include "nm_otool.h"
 #include "nm.h"
 
-static void			print_nm(t_nm_otool *nm_otool, struct symtab_command *symtab)
+static bool			print_nm(t_nm_otool *nm_otool, struct symtab_command *symtab)
 {
 	uint32_t		i;
+	char			*str;
 	char			*stringtable;
 	struct nlist_64	*array;
 
-	array = nm_otool->file.memory + symtab->symoff;
-	stringtable = nm_otool->file.memory + symtab->stroff;
 	i = 0;
+	if(!(array = (struct nlist_64 *)get_safe_address(nm_otool, (char *)nm_otool->file.memory + symtab->symoff)))
+		return (false);
+	if(!(stringtable = (char *)get_safe_address(nm_otool, (char *)nm_otool->file.memory + symtab->stroff)))
+		return (false);
 	while (i < symtab->nsyms)
 	{
-		ft_printf("%s\n",stringtable + array[i].n_un.n_strx);
+		if (!(str = (char *)get_safe_address(nm_otool, (char *)stringtable + array[i].n_un.n_strx)))
+			return (false);
+		ft_printf("%s\n", str);
 		i++;
 	}
+	return (true);
 }
 
 bool				mach_o_64_obj_handler(t_nm_otool *nm_otool)
@@ -40,7 +46,7 @@ bool				mach_o_64_obj_handler(t_nm_otool *nm_otool)
 			if(!(symtab = (struct symtab_command *)get_safe_address(nm_otool, (char *)lc))
 				|| !get_safe_address(nm_otool, (char *)lc + sizeof(*symtab)))
 				return (false);
-			print_nm(nm_otool, symtab);
+			return (print_nm(nm_otool, symtab));
 			break ;
 		}
 		if (!(lc = (t_lc *)get_safe_address(nm_otool, (char *)lc + lc->cmdsize)))
