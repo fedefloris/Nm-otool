14c05f164d4c48519681e0c123c618eea30f4c13
kernel_j <jenniferwong89@gmail.com>
2018-11-23 18:03:46 +0100

    rework handling of fat64 to handle fat64 and fat32

--- otool/srcs/main/fat.c
+++ otool/srcs/main/fat.c
@@ -1,27 +1,56 @@
 #include <mach-o/fat.h>
+#include <mach/machine.h>
 #include "ft_printf.h"
 #include "nm_otool.h"
 #include "otool.h" 
 
-int fat_64(t_file *file)
+int process_fat(struct fat_arch *arch, uint32_t nfat_arch, t_file *file,\
+        int (*macho)(t_file *file))
 {
-    t_file              fat;
-    struct fat_header   *header;
-    struct fat_arch     *arch;
-    uint32_t            nfat_arch;
-    uint32_t            i;
+    struct fat_arch *fat_ptr;
+    uint32_t        i;
+    t_file          fat;
 
-    header = (struct fat_header *)file->map;
-    nfat_arch = swap_endian(header->nfat_arch);
-    arch = (struct fat_arch *)((void *)file->map + sizeof(struct fat_header));
+    fat_ptr = arch;
     i = 0;
     while (i < nfat_arch)
     {
         ft_bzero(&fat, sizeof(t_file));
-        fat.map = (void *)file->map + swap_endian(arch->offset);
-        macho_64(&fat);
-        arch = (struct fat_arch *)((void *)arch + sizeof(struct fat_arch));
+        fat.map = (void *)file->map + swap_endian(fat_ptr->offset);
+        macho(&fat);
+        fat_ptr = (struct fat_arch *)((void *)fat_ptr + sizeof(struct fat_arch));
         i++;
     }
+    return (SUCCESS);
+}
+
+int handle_fat(struct fat_arch *arch, uint32_t nfat_arch, t_file *file)
+{
+    int ret;
+
+    ret = FAILURE;
+    if (swap_endian(arch->cputype) == CPU_TYPE_X86_64)
+    {
+        process_fat(arch, nfat_arch, file, &macho_64);
+        ret = SUCCESS;
+    }
+    else if (swap_endian(arch->cputype) == CPU_TYPE_I386)
+    {
+        process_fat(arch, nfat_arch, file, &macho_32);
+        ret = SUCCESS;
+    }
+    return (ret);
+}
+
+int fat(t_file *file)
+{
+    struct fat_header   *header;
+    struct fat_arch     *arch;
+    uint32_t            nfat_arch;
+
+    header = (struct fat_header *)file->map;
+    nfat_arch = swap_endian(header->nfat_arch);
+    arch = (struct fat_arch *)((void *)file->map + sizeof(struct fat_header));
+    handle_fat(arch, nfat_arch, file);
     return (SUCCESS);
 }
