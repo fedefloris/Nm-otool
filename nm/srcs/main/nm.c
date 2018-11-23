abb7d4e34c110b927c8c25108e2f756193088701
Dav Højt <davhojt@gmail.com>
2018-11-23 05:20:44 +0100

    added comment to explain function.

--- nm/srcs/main/nm.c
+++ nm/srcs/main/nm.c
@@ -5,7 +5,8 @@ int					main(int argc, char **argv, char **env)
 {
 	t_nm_otool	nm_otool;
 
-	config_nm_otool(&nm_otool, argv, env);
+	if (!config_nm_otool(&nm_otool, argv, env))
+		return (EXIT_FAILURE);
 	if (argc == 1)
 		return (list_obj_symbols(&nm_otool, DEFAULT_ARGUMENT, &obj_handler));
 	return (list_objs_symbols(&nm_otool, &obj_handler));
