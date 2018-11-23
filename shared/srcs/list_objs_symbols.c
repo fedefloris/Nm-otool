613de136f6a87b588c98e05886bc2fc3192e3b8e
Dav Højt <davhojt@gmail.com>
2018-11-23 04:48:18 +0100

    list_obs_symbols now starts on the first REAL (non-option) argv.

--- shared/srcs/list_objs_symbols.c
+++ shared/srcs/list_objs_symbols.c
@@ -7,10 +7,11 @@ int		list_objs_symbols(t_nm_otool *nm_otool, t_obj_handler obj_handler)
 
 	argv = nm_otool->argv;
 	exit_status = EXIT_SUCCESS;
-	while (*(++argv))
+	while (*argv)
 	{
 		if (list_obj_symbols(nm_otool, *argv, obj_handler) == EXIT_FAILURE)
 			exit_status = EXIT_FAILURE;
+		argv++;
 	}
 	return (exit_status);
 }
