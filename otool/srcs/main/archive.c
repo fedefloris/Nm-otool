1c1ae3f9f5a809e1f48534f356439b86e8d68a6d
Jennifer WONG <jwong@vente-privee.com>
2018-11-26 17:46:09 +0100

    add correct padding to archive

--- otool/srcs/main/archive.c
+++ otool/srcs/main/archive.c
@@ -2,11 +2,22 @@
 #include "otool.h"
 #include "ft_printf.h"
 
+int     get_ar_name_length(char  *ar_name)
+{
+    char    *ar_name_len;
+
+    ar_name_len = ft_strstr(ar_name, "/");
+    if (ar_name_len)
+        return (ft_atoi(ar_name_len + 1));
+    return (-1);
+}
+
 int     archive(t_file *file)
 {
     struct ar_hdr   *header;
     struct ar_hdr   *ar_ptr;
     int             ar_size;
+    int             ar_name_len;
     char            *filename;
     t_file          ar;
 
@@ -19,9 +30,10 @@ int     archive(t_file *file)
         ar_size = ft_atoi(ar_ptr->ar_size);
         filename = (char *)ar_ptr + sizeof(struct ar_hdr);
         ft_printf("%s\n", filename);
+        ar_name_len = get_ar_name_length(ar_ptr->ar_name);
         ft_bzero(&ar, sizeof(t_file));
         ar.map = (void *)ar_ptr + sizeof(struct ar_hdr)\
-                 + ft_strlen(filename) + 1 + (sizeof(int) * 2);
+                 + ar_name_len;
         otool(&ar);
         if ((void *)(ar_ptr = (void *)ar_ptr + ar_size + sizeof(struct ar_hdr)) >= (void *)file->map + file->file_size)
             ar_ptr = NULL;
