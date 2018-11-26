e0ec49a29f1c44dde32c3f9f4863843e62fdd4a3
Dav Højt <davhojt@gmail.com>
2018-11-26 01:10:45 +0100

    Created bad logic in avoiding linked lists Currently only record the last section count of text, bss and data.

--- nm/srcs/mach_o/mach_o_64_obj_handler.c
+++ nm/srcs/mach_o/mach_o_64_obj_handler.c
@@ -1,7 +1,7 @@
 #include "nm_otool.h"
 #include "nm.h"
 
-static char			get_type_64(uint8_t n_type, u_int64_t n_value)
+static char			get_type_64(uint8_t n_type, u_int64_t n_value, u_int8_t n_sect, t_section sections)
 {
 	char			type;
 
@@ -12,8 +12,14 @@ static char			get_type_64(uint8_t n_type, u_int64_t n_value)
 		type = 'A';
 	else if ((n_type & N_TYPE) == N_PBUD)
 		type = 'U';
-	//else if ((n_type & N_TYPE) == N_SECT)
-	//	type = ???;
+	else if ((n_type & N_TYPE) == N_SECT)
+	{
+		ft_printf("Sect %d, Bss %d, data %d, text %d\n", n_sect, sections.bss, sections.data, sections.text);
+		type = (n_sect == sections.bss) ? 'B' : type;
+		type = (n_sect == sections.data) ? 'D' : type;
+		type = (n_sect == sections.text) ? 'T' : type;
+		type = (type == '0') ? 'S' : type;
+	}
 	else if ((n_type & N_TYPE) == N_INDR)
 		type = 'I';
 	if ((n_type & N_STAB) != 0)
@@ -23,7 +29,7 @@ static char			get_type_64(uint8_t n_type, u_int64_t n_value)
 	return (type);
 }
 
-static bool			get_symbols_64(t_nm_otool *nm_otool, struct symtab_command *symtab)
+static bool			get_symbols_64(t_nm_otool *nm_otool, struct symtab_command *symtab, t_section sections)
 {
 	uint32_t		i;
 	char			*str;
@@ -44,7 +50,7 @@ static bool			get_symbols_64(t_nm_otool *nm_otool, struct symtab_command *symtab
 		if (!string_is_safe(nm_otool, (char *)str))
 			return (false);
 		ft_printf("%16.16x ", array[i].n_value);
-		ft_printf("%c (%u) ", get_type_64(array[i].n_type, array[i].n_value), array[i].n_sect);
+		ft_printf("%c (%u) ", get_type_64(array[i].n_type, array[i].n_value, array[i].n_sect, sections), array[i].n_sect);
 		ft_printf("%s\n", str);
 		i++;
 	}
@@ -111,6 +117,6 @@ bool				mach_o_64_obj_handler(t_nm_otool *nm_otool)
 			return (false);
 	}
 	if (symtab)
-		return (get_symbols_64(nm_otool, symtab));
+		return (get_symbols_64(nm_otool, symtab, sections));
 	return (true);
 }
