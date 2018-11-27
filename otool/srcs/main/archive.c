91c7175f9ec4b21a4279bc4ee2e1a14b5a5f083b
Jennifer WONG <jwong@vente-privee.com>
2018-11-27 11:28:07 +0100

    rework archive

--- otool/srcs/main/archive.c
+++ otool/srcs/main/archive.c
@@ -12,31 +12,39 @@ int     get_ar_name_length(char  *ar_name)
     return (-1);
 }
 
-int     archive(t_file *file)
+int     handle_archive_objects(t_file *file, struct ar_hdr *ar_ptr)
 {
-    struct ar_hdr   *header;
-    struct ar_hdr   *ar_ptr;
     int             ar_size;
     int             ar_name_len;
     char            *filename;
     t_file          ar;
 
-    header = (struct ar_hdr *)((void *)file->map + SARMAG);
-    ar_size = ft_atoi(header->ar_size);
-    ar_ptr = (struct ar_hdr *)((void *)file->map + SARMAG\
-            + ar_size + sizeof(struct ar_hdr));
     while (ar_ptr)
     {
         ar_size = ft_atoi(ar_ptr->ar_size);
         filename = (char *)ar_ptr + sizeof(struct ar_hdr);
-        ft_printf("%s\n", filename);
+        print_filename(file, filename);
         ar_name_len = get_ar_name_length(ar_ptr->ar_name);
         ft_bzero(&ar, sizeof(t_file));
         ar.map = (void *)ar_ptr + sizeof(struct ar_hdr)\
                  + ar_name_len;
+        ar.filetype = ARCHIVE;
         otool(&ar);
         if ((void *)(ar_ptr = (void *)ar_ptr + ar_size + sizeof(struct ar_hdr)) >= (void *)file->map + file->file_size)
             ar_ptr = NULL;
     }
     return (SUCCESS);
+}
+
+int     archive(t_file *file)
+{
+    struct ar_hdr   *header;
+    struct ar_hdr   *ar_ptr;
+    int             ar_size;
+
+    header = (struct ar_hdr *)((void *)file->map + SARMAG);
+    ar_size = ft_atoi(header->ar_size);
+    ar_ptr = (struct ar_hdr *)((void *)file->map + SARMAG\
+            + ar_size + sizeof(struct ar_hdr));
+    return (handle_archive_objects(file, ar_ptr));
 }
