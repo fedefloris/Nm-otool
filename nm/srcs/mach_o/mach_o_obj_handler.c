f7714b98e558689553ad31ddf50a6f0f8650a5ef
Dav Højt <davhojt@gmail.com>
2018-11-29 23:35:23 +0100

    refactor function names.

--- nm/srcs/mach_o/mach_o_obj_handler.c
+++ nm/srcs/mach_o/mach_o_obj_handler.c
@@ -5,5 +5,7 @@ bool		mach_o_obj_handler(t_nm_otool *nm_otool)
 {
 	if (nm_otool->file.format == MACH_O_32_FORMAT)
 		return (mach_o_32_obj_handler(nm_otool));
-	return (mach_o_64_obj_handler(nm_otool));
+	if (nm_otool->file.format == MACH_O_64_FORMAT)
+		return (mach_o_64_obj_handler(nm_otool));
+	return (false);
 }
