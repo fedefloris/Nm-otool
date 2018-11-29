4d8e1f7a69612a2ab6e13bff057c6879126ce6cc
Dav HOJT <davhojt@gmail.com>
2018-11-29 14:56:07 +0100

    broke out loop of get type into own function.

--- nm/srcs/mach_o/mach_o_64_obj_handler.c
+++ nm/srcs/mach_o/mach_o_64_obj_handler.c
@@ -1,18 +1,9 @@
 #include "nm_otool.h"
 #include "nm.h"
 
-static char			get_type_64(uint8_t n_type, u_int64_t n_value, u_int8_t n_sect, t_section *sections)
+static char			get_type_section_64(char type, u_int8_t n_sect, t_section *sections)
 {
-	char			type;
-
-	type = '0';
-	type = ((n_type & N_TYPE) == N_UNDF && n_value) ? 'C' : type;
-	type = ((n_type & N_TYPE) == N_UNDF && !n_value) ? 'U' : type;
-	type = ((n_type & N_TYPE) == N_ABS && !n_value) ? 'A' : type;
-	type = ((n_type & N_TYPE) == N_PBUD && !n_value) ? 'U' : type;
-	if ((n_type & N_TYPE) == N_SECT)
-	{
-		while (sections)
+	while (sections)
 		{
 			if (n_sect == sections->sec_number)
 				break ;
@@ -26,6 +17,21 @@ static char			get_type_64(uint8_t n_type, u_int64_t n_value, u_int8_t n_sect, t_
 			type = (!ft_strcmp(sections->name, SECT_DATA)) ? 'D' : type;
 			type = (!ft_strcmp(sections->name, SECT_TEXT)) ? 'T' : type;
 		}
+		return (type);
+}
+
+static char			get_type_64(uint8_t n_type, u_int64_t n_value, u_int8_t n_sect, t_section *sections)
+{
+	char			type;
+
+	type = '0';
+	type = ((n_type & N_TYPE) == N_UNDF && n_value) ? 'C' : type;
+	type = ((n_type & N_TYPE) == N_UNDF && !n_value) ? 'U' : type;
+	type = ((n_type & N_TYPE) == N_ABS && !n_value) ? 'A' : type;
+	type = ((n_type & N_TYPE) == N_PBUD && !n_value) ? 'U' : type;
+	if ((n_type & N_TYPE) == N_SECT)
+	{
+		type = get_type_section_64(type, n_sect, sections);
 	}
 	type = ((n_type & N_TYPE) == N_INDR && !n_value) ? 'I' : type;
 	type = ((n_type & N_STAB) != 0) ? '-' : type;
