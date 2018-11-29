f145ad0af10794246bca7eac48149b439e1c314b
Dav Højt <davhojt@gmail.com>
2018-11-29 23:45:20 +0100

    Added fat 32 to handler.

---
+++ nm/srcs/mach_o/mach_fat_32_obj_handler.c
@@ -0,0 +1,8 @@
+#include "nm_otool.h"
+#include "nm.h"
+
+bool		mach_fat_32_obj_handler(t_nm_otool *nm_otool)
+{
+	ft_printf("Mach-fat-32, File_name: %s\n", nm_otool->file.name);
+	return (true);
+}
