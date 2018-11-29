9c6feac22be2d46d516e2aabd078c8f5ed01f759
Dav Højt <davhojt@gmail.com>
2018-11-29 23:45:20 +0100

    Added fat 32 to handler.

--- nm/srcs/mach_o/mach_o_obj_handler.c
+++ nm/srcs/mach_o/mach_o_obj_handler.c
@@ -7,5 +7,9 @@ bool		mach_o_obj_handler(t_nm_otool *nm_otool)
 		return (mach_o_32_obj_handler(nm_otool));
 	if (nm_otool->file.format == MACH_O_64_FORMAT)
 		return (mach_o_64_obj_handler(nm_otool));
+	if (nm_otool->file.format == MACH_O_FAT_32)
+		return (mach_fat_32_obj_handler(nm_otool));
+	if (nm_otool->file.format == MACH_O_FAT_64)
+		return (mach_fat_64_obj_handler(nm_otool));
 	return (false);
 }
