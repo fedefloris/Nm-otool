29573937b4ac4338ae0fb07cf77382a56a5ad2b7
Dav HOJT <davhojt@gmail.com>
2018-11-29 14:24:22 +0100

    normed

--- nm/srcs/mach_o/display_symbols.c
+++ nm/srcs/mach_o/display_symbols.c
@@ -6,7 +6,9 @@ void				display_symbols(t_nm_otool *nm_otool, t_symbol *symbols)
 	while (symbols)
 	{
 		symbols = sort_symbols(nm_otool, symbols);
-		(symbols->value) ? ft_printf("%-17.16jx", symbols->value) : ft_printf("%17s", "");
+		(symbols->value)
+			? ft_printf("%-17.16jx", symbols->value)
+			: ft_printf("%17s", "");
 		ft_printf("%c %s\n", symbols->type, symbols->name);
 		symbols = symbols->next;
 	}
