8c4a4f5d40995bd4121ba178667ebffccc5175f3
Dav HOJT <davhojt@gmail.com>
2018-11-30 17:09:27 +0100

    changed display of mach-o 32 to display value as 8 characters.

--- nm/srcs/mach_o/display_symbols.c
+++ nm/srcs/mach_o/display_symbols.c
@@ -6,9 +6,18 @@ void				display_symbols(t_nm_otool *nm_otool, t_symbol *symbols)
 	while (symbols)
 	{
 		symbols = sort_symbols(nm_otool, symbols);
-		(symbols->value)
-			? ft_printf("%-17.16jx", symbols->value)
-			: ft_printf("%17s", "");
+		if (nm_otool->file.format == MACH_O_32_FORMAT)
+		{
+			(symbols->value)
+				? ft_printf("%-9.8jx", symbols->value)
+				: ft_printf("%9s", "");
+		}
+		else
+		{
+			(symbols->value)
+				? ft_printf("%-17.16jx", symbols->value)
+				: ft_printf("%17s", "");
+		}
 		ft_printf("%c %s\n", symbols->type, symbols->name);
 		symbols = symbols->next;
 	}
