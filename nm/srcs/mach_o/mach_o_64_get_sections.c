3b49998a56bdad26fbf1234039c0bed0b1e45afa
Dav Højt <davhojt@gmail.com>
2018-11-30 10:25:12 +0100

    64 now uses common create_section

--- nm/srcs/mach_o/mach_o_64_get_sections.c
+++ nm/srcs/mach_o/mach_o_64_get_sections.c
@@ -1,25 +1,6 @@
 #include "nm_otool.h"
 #include "nm.h"
 
-static bool			mach_o_64_create_section(t_section **sections,
-		struct section_64 *sec, unsigned char sec_number)
-{
-	t_section				*new;
-
-	if (!(new = (t_section *)ft_memalloc(sizeof(t_section))))
-		return (false);
-	new->name = sec->sectname;
-	new->sec_number = sec_number;
-	if (!*sections)
-		*sections = new;
-	else
-	{
-		new->next = *sections;
-		*sections = new;
-	}
-	return (true);
-}
-
 bool				mach_o_64_get_sections(t_nm_otool *nm_otool,
 		t_section **sections, struct segment_command_64 *segment)
 {
@@ -40,7 +21,7 @@ bool				mach_o_64_get_sections(t_nm_otool *nm_otool,
 		if (!ft_strcmp(sec->sectname, SECT_DATA)
 				|| !ft_strcmp(sec->sectname, SECT_BSS)
 				|| !ft_strcmp(sec->sectname, SECT_TEXT))
-			if (!(mach_o_64_create_section(sections, sec, sec_number)))
+			if (!(mach_o_create_section(sections, sec->sectname, sec_number)))
 				return (false);
 		if (!(sec = (struct section_64 *)get_safe_address(
 				nm_otool, (char *)sec + sizeof(*sec))))
