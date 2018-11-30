1aee3d086e0300dfc12dbd3659b82d04d16a57ee
Dav Højt <davhojt@gmail.com>
2018-11-30 10:19:58 +0100

    created get_sections for 32.

---
+++ nm/srcs/mach_o/mach_o_32_get_sections.c
@@ -0,0 +1,51 @@
+#include "nm_otool.h"
+#include "nm.h"
+
+static bool			mach_o_create_section(t_section **sections,
+		char *sectname, unsigned char sec_number)//break out of 64 and 32
+{
+	t_section				*new;
+
+	if (!(new = (t_section *)ft_memalloc(sizeof(t_section))))
+		return (false);
+	new->name = sectname;
+	new->sec_number = sec_number;
+	if (!*sections)
+		*sections = new;
+	else
+	{
+		new->next = *sections;
+		*sections = new;
+	}
+	return (true);
+}
+
+bool				mach_o_32_get_sections(t_nm_otool *nm_otool,
+		t_section **sections, struct segment_command *segment)
+{
+	uint32_t				i;
+	static unsigned char	sec_number = 1;
+	struct section			*sec;
+
+	i = 0;
+	if (!(sec = (struct section *)get_safe_address(
+			nm_otool, (char *)segment + sizeof(*segment))))
+		return (false);
+	while (i++ < segment->nsects)
+	{
+		if (!get_safe_address(nm_otool, (char *)sec + sizeof(*sec)))
+			return (false);
+		if (!string_is_safe(nm_otool, (char *)sec->sectname))
+			return (false);
+		if (!ft_strcmp(sec->sectname, SECT_DATA)
+				|| !ft_strcmp(sec->sectname, SECT_BSS)
+				|| !ft_strcmp(sec->sectname, SECT_TEXT))
+			if (!(mach_o_create_section(sections, sec->sectname, sec_number)))
+				return (false);
+		if (!(sec = (struct section *)get_safe_address(
+				nm_otool, (char *)sec + sizeof(*sec))))
+			return (false);
+		sec_number++;
+	}
+	return (true);
+}
