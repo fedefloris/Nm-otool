b90009ee0a70046461c416fdb5b1a62c3e7a26d1
fedefloris <fzfloris@gmail.com>
2018-12-01 15:40:25 +0100

    Print symbols for ELF-64

--- nm/srcs/elf/elf_64_obj_handler.c
+++ nm/srcs/elf/elf_64_obj_handler.c
@@ -2,27 +2,44 @@
 #include "nm.h"
 #include <elf.h>
 
-bool		elf_64_obj_handler(t_nm_otool *nm_otool)
+bool				elf_64_obj_handler(t_nm_otool *nm_otool)
 {
-	// Elf64_Shdr	*str_section;
+	char				*str_section;
 	Elf64_Shdr	*section;
 	Elf64_Ehdr	*header;
+	Elf64_Sym		*sym;
+	uint64_t		symbols_count;
 	uint16_t		i;
+	unsigned char	bind;
+	unsigned char	type;
 
 	i = 0;
 	header = (Elf64_Ehdr*)nm_otool->file.memory;
 	ft_printf("Elf64, File_name: %s\n", nm_otool->file.name);
-	ft_printf("Section header offset: %lu\n", header->e_shoff);
 	section = (Elf64_Shdr*)((char*)nm_otool->file.memory + header->e_shoff);
-	// str_section = (Elf64_Shdr*)((char*)nm_otool->file.memory +
-	// (header->e_shoff + (header->e_shstrndx * sizeof(*section))));
 	while (i < header->e_shnum)
 	{
-		section = (Elf64_Shdr*)((char*)section + sizeof(*section));
-		if ((section->sh_type == SHT_SYMTAB)
-			|| (section->sh_type == SHT_DYNSYM))
+		if ((section[i].sh_type == SHT_SYMTAB)
+			|| (section[i].sh_type == SHT_DYNSYM))
 		{
-			ft_printf("Section n. %u is a Symbolic table\n", i);
+			sym = (Elf64_Sym*)(((char*)nm_otool->file.memory) + section[i].sh_offset);
+			str_section = (char*)nm_otool->file.memory + section[section[i].sh_link].sh_offset;
+			symbols_count = section[i].sh_size / sizeof(Elf64_Sym);
+			while (symbols_count--)
+			{
+				bind = ELF32_ST_BIND(sym->st_info);
+				type = ELF32_ST_TYPE(sym->st_info);
+				ft_printf("%016x ", sym->st_value);
+				(void)bind;
+				if (bind == STB_LOCAL && type == STT_FUNC)
+					ft_putstr("t ");
+				else if (bind == STB_GLOBAL && type == STT_FUNC)
+					ft_putstr("T ");
+				else if (type == STT_NOTYPE)
+					ft_putstr("U ");
+				ft_printf("%s\n", str_section + sym->st_name);
+				sym++;
+			}
 		}
 		i++;
 	}
