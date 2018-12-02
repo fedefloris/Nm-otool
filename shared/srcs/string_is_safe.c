19572def9fd0189c03d9c682ea0f183fea961317
Dav Højt <davhojt@gmail.com>
2018-12-02 23:52:19 +0100

    Modified string_is_safe to test against ft_isprint

--- shared/srcs/string_is_safe.c
+++ shared/srcs/string_is_safe.c
@@ -4,11 +4,12 @@ bool				string_is_safe(t_nm_otool *nm_otool, char *str)
 {
 	if (!str)
 		return (false);
-	while (get_safe_address(nm_otool, str)
-		&& ft_isalpha(*str))
+	while (get_safe_address(nm_otool, str))
 	{
 		if (!*str)
 			return (true);
+		if (!ft_isprint(*str))
+			return (false);
 		str++;
 	}
 	return (false);
