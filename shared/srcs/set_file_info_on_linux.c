4bfbdf37d41e01fbb0a08ce1630885630dc6c4f6
fedefloris <fzfloris@gmail.com>
2018-12-04 23:09:02 +0100

    Code refactoring

--- shared/srcs/set_file_info_on_linux.c
+++ shared/srcs/set_file_info_on_linux.c
@@ -16,13 +16,9 @@ static bool	set_format(t_nm_otool *nm_otool, Elf32_Ehdr *header)
 	if (header->e_ident[EI_CLASS] == ELFCLASS32)
 		nm_otool->file.format = ELF_32;
 	else if (header->e_ident[EI_CLASS] == ELFCLASS64)
-	{
 		nm_otool->file.format = ELF_64;
-		if (nm_otool->file.size < (long)sizeof(Elf64_Ehdr))
-			return (ERROR_LOG("Bad size"));
-	}
 	else
-		return (ERROR_LOG("Architecture not supported"));
+		return (false);
 	return (true);
 }
 
@@ -36,15 +32,17 @@ bool				set_file_info_on_linux(t_nm_otool *nm_otool)
 {
 	Elf32_Ehdr	*header;
 
-	header = (Elf32_Ehdr*)nm_otool->file.memory;
-	if (nm_otool->file.size < (long)sizeof(Elf32_Ehdr))
+	if (!SET(header, nm_otool->file.memory) || !STRUCT_IS_SAFE(header))
 		return (ERROR_LOG("Bad size"));
 	else if (!has_good_ELF_magic_number(header))
 		return (ERROR_LOG("Bad magic number"));
 	else if (!has_good_version(header))
 		return (ERROR_LOG("Wrong version"));
 	else if (!set_format(nm_otool, header))
-		return (false);
+		return (ERROR_LOG("Architecture not supported"));
+	else if (IS_ELF_64(nm_otool->file.format)
+		&& nm_otool->file.size < (long)sizeof(Elf64_Ehdr))
+		return (ERROR_LOG("Bad size"));
 	else if (!set_endianness(nm_otool, header))
 		return (ERROR_LOG("Bad endianness"));
 	return (true);
