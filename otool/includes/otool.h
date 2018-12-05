05c91752af16cf834c4a143a4dd2a5742c592639
Dav Højt <davhojt@gmail.com>
2018-12-05 23:03:57 +0100

    Trnaslated t_file2 into t_file

--- otool/includes/otool.h
+++ otool/includes/otool.h
@@ -10,48 +10,40 @@
 # define ARCHIVE 'a'
 # define FAT 'f'
 
-typedef struct  s_file2
-{
-    size_t      file_size;
-    char        *map;
-    char        *filename;
-    char        filetype;
-}               t_file2;
-
 /*
 ** archive.c
 */
-int             archive(t_file2 *file);
+int             archive(t_file *file);
 
 /*
 ** fat_64.c
 */
-int             fat(t_file2 *file);
+int             fat(t_file *file);
 
 /*
 ** load_file.c
 */
-int             load_file(char *filename, t_file2 *file);
+int             load_file(char *filename, t_file *file);
 
 /*
 ** macho_32.c
 */
-int             macho_32(t_file2 *file);
+int             macho_32(t_file *file);
 
 /*
 ** macho_64.c
 */
-int             macho_64(t_file2 *file);
+int             macho_64(t_file *file);
 
 /*
 **  otool.c
 */
-int             otool(t_file2 *file);
+int             otool(t_file *file);
 
 /*
 ** print.c
 */
-void            print_filename(t_file2 *file, char *current_file);
+void            print_filename(t_file *file, char *current_file);
 
 /*
 ** swap.c
--- otool/srcs/main/archive.c
+++ otool/srcs/main/archive.c
@@ -12,12 +12,12 @@ int     get_ar_name_length(char  *ar_name)
     return (-1);
 }
 
-int     handle_archive_objects(t_file2 *file, struct ar_hdr *ar_ptr)
+int     handle_archive_objects(t_file *file, struct ar_hdr *ar_ptr)
 {
     int             ar_size;
     int             ar_name_len;
     char            *filename;
-    t_file2          ar;
+    t_file          ar;
 
     while (ar_ptr)
     {
@@ -25,26 +25,26 @@ int     handle_archive_objects(t_file2 *file, struct ar_hdr *ar_ptr)
         filename = (char *)ar_ptr + sizeof(struct ar_hdr);
         print_filename(file, filename);
         ar_name_len = get_ar_name_length(ar_ptr->ar_name);
-        ft_bzero(&ar, sizeof(t_file2));
+        ft_bzero(&ar, sizeof(t_file));
         ar.map = (void *)ar_ptr + sizeof(struct ar_hdr)\
                  + ar_name_len;
         ar.filetype = ARCHIVE;
         otool(&ar);
-        if ((void *)(ar_ptr = (void *)ar_ptr + ar_size + sizeof(struct ar_hdr)) >= (void *)file->map + file->file_size)
+        if ((void *)(ar_ptr = (void *)ar_ptr + ar_size + sizeof(struct ar_hdr)) >= (void *)file->memory + file->size)
             ar_ptr = NULL;
     }
     return (SUCCESS);
 }
 
-int     archive(t_file2 *file)
+int     archive(t_file *file)
 {
     struct ar_hdr   *header;
     struct ar_hdr   *ar_ptr;
     int             ar_size;
 
-    header = (struct ar_hdr *)((void *)file->map + SARMAG);
+    header = (struct ar_hdr *)((void *)file->memory + SARMAG);
     ar_size = ft_atoi(header->ar_size);
-    ar_ptr = (struct ar_hdr *)((void *)file->map + SARMAG\
+    ar_ptr = (struct ar_hdr *)((void *)file->memory + SARMAG\
             + ar_size + sizeof(struct ar_hdr));
     return (handle_archive_objects(file, ar_ptr));
 }
