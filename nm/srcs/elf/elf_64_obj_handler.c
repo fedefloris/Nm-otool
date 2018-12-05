eb212bc40eb0cac59e62946c653c8b2c4ddb94c7
Federico <fzfloris@gmail.com>
2018-12-05 11:05:25 +0100

    Protect more parse_section_headers inside elf_64_obj_handler

--- nm/srcs/elf/elf_64_obj_handler.c
+++ nm/srcs/elf/elf_64_obj_handler.c
@@ -36,21 +36,25 @@ static bool	parse_section_header(t_nm_otool *nm_otool,
 static bool	parse_section_headers(t_nm_otool *nm_otool,
 	Elf64_Ehdr *header)
 {
-	Elf64_Shdr	*section_header;
+	Elf64_Shdr	*section_headers;
 	char				*str_section;
+	Elf64_Off		sh_offset;
 	uint16_t		i;
 
 	// Check if e_shnum is larger than or equal to SHN_LORESERVE (man elf)
-	if (!SET(section_header, (char*)header + header->e_shoff))
+	if (!SET(section_headers, (char*)header + header->e_shoff))
 		return (ERROR_LOG("Not enough space for the first section header"));
 	i = 0;
 	while (i < header->e_shnum)
 	{
-		if (!STRUCT_IS_SAFE(&section_header[i]))
+		if (!STRUCT_IS_SAFE(&section_headers[i]))
 			return (ERROR_LOG("Not enough space for an other section header"));
-		// Need to fix and protect it
-		str_section = (char*)header + (&section_header[section_header->sh_link])->sh_offset;
-		parse_section_header(nm_otool, section_header + i, str_section);
+		if (!STRUCT_IS_SAFE(&section_headers[section_headers[i].sh_link]))
+			return (ERROR_LOG("sh_link index is outside the section headers array"));
+		sh_offset = section_headers[section_headers[i].sh_link].sh_offset;
+		if (!SET(str_section, (char*)header + sh_offset))
+			return (ERROR_LOG("Not enough space for the string table"));
+		parse_section_header(nm_otool, &section_headers[i], str_section);
 		i++;
 	}
 	return (true);
@@ -63,7 +67,7 @@ bool				elf_64_obj_handler(t_nm_otool *nm_otool)
 	if (!SET(header, nm_otool->file.memory)
 		|| !STRUCT_IS_SAFE(header))
 		return (false);
-	if (header->e_shoff == 0 || header->e_shnum == 0)
+	if (header->e_shoff == 0)
 		return (true);
 	if (!parse_section_headers(nm_otool, header))
 		return (false);
