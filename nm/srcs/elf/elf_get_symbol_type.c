d650e13e7aaa204231626cce5627caae809d76ec
fedefloris <fzfloris@gmail.com>
2018-12-05 23:05:12 +0100

    Return basic types on elf_get_symbol_type

--- nm/srcs/elf/elf_get_symbol_type.c
+++ nm/srcs/elf/elf_get_symbol_type.c
@@ -9,10 +9,10 @@ char		elf_get_symbol_type(unsigned char st_info)
 	bind = ELF32_ST_BIND(st_info);
 	type = ELF32_ST_TYPE(st_info);
 	if (bind == STB_LOCAL && type == STT_FUNC)
-		;// ft_putstr("t ");
+		return ('t');
 	else if (bind == STB_GLOBAL && type == STT_FUNC)
-		;// ft_putstr("T ");
+		return ('T');
 	else if (type == STT_NOTYPE)
-		;	// ft_putstr("U ");
+		return ('U');
 	return ('U');
 }
