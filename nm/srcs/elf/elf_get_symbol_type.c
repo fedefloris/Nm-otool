1ebdfeb15f1aec845a68ba37957260ad8531b4b1
fedefloris <fzfloris@gmail.com>
2018-12-06 00:27:19 +0100

    Add more types in comments

--- nm/srcs/elf/elf_get_symbol_type.c
+++ nm/srcs/elf/elf_get_symbol_type.c
@@ -3,16 +3,34 @@
 
 char		elf_get_symbol_type(unsigned char st_info)
 {
-	unsigned char	bind;
-	unsigned char	type;
+	unsigned char	st_bind;
+	unsigned char	st_type;
+	char					type;
 
-	bind = ELF32_ST_BIND(st_info);
-	type = ELF32_ST_TYPE(st_info);
-	if (bind == STB_LOCAL && type == STT_FUNC)
-		return ('t');
-	else if (bind == STB_GLOBAL && type == STT_FUNC)
-		return ('T');
-	else if (type == STT_NOTYPE)
-		return ('U');
-	return ('U');
+	st_bind = ELF32_ST_BIND(st_info);
+	st_type = ELF32_ST_TYPE(st_info);
+	(void)st_bind;
+	(void)st_type;
+	type = 'U';
+	// if (st_type == STT_NOTYPE)
+	// 	type = 'U';
+	// else if (sym->st_shndx == SHN_ABS)
+    // type = 'A';
+	// else if (section[sym->st_shndx].sh_type == SHT_NOBITS)
+    // type = 'B';
+	// else if (sym->st_shndx == SHN_COMMON)
+    // type = 'C';
+	// else if (section[sym->st_shndx].sh_type == SHT_PROGBITS)
+    // type = 'D';
+	// else if ()
+    // type = 'G';
+	// else if ()
+    // type = 'i';
+	// else if ()
+    // type = 'I';
+	// else if ()
+    // type = 'N';
+	if (st_bind == STB_LOCAL)
+		type += 32;
+	return (type);
 }
