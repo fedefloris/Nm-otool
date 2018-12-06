cccab8837439b70cbe7e0c83f1bcf6a78dac62c8
Dav HOJT <davhojt@gmail.com>
2018-12-06 17:25:58 +0100

    Added making_the_best_ever_universal_endian_reverser.c

---
+++ making_the_best_ever_universal_endian_reverser.c
@@ -0,0 +1,11 @@
+#include <mach-o/fat.h>
+
+int					main(void)
+{
+	uint8_t			x8 = 0xff;
+	uint16_t		x16 = 0xff00;
+	uint32_t		x32 = 0xff884400;
+	uint64_t		x64 = 0xffaa997755331100;
+
+	return (0);
+}
