5b91a82734cb715e5a61ff13b95d924eec94e34e
Dav Højt <davhojt@gmail.com>
2018-12-06 01:44:34 +0100

    Exchanged otool's main for generic main.

--- otool/srcs/main/otool.c
+++ otool/srcs/main/otool.c
@@ -1,44 +1,12 @@
 #include "nm_otool.h"
 #include "otool.h"
-#include <sys/stat.h>
-#include <sys/mman.h>
 
-int					otool(t_file *file)
+int					main(int argc, char **argv, char **env)
 {
-    uint32_t    magic;
+	t_nm_otool	nm_otool;
 
-    magic = *(uint32_t *)file->memory;
-    if (magic == MH_MAGIC_64 || magic == MH_CIGAM_64)
-        macho_64(file);
-    else if (magic == MH_MAGIC || magic == MH_CIGAM)
-        macho_32(file);
-    else if (magic == FAT_MAGIC || magic == FAT_CIGAM)
-        fat(file);
-    else if (ft_strncmp((char *)file->memory, ARMAG, SARMAG) == 0)
-    {
-        ft_printf("Archive : %s\n", file->name);
-        file->format = ARCHIVE;
-        archive(file);
-    }
-    return (SUCCESS);
-}
-
-int					main(int argc, char **argv)
-{
-    int     i;
-    int     ret;
-    t_file  file;
-
-    i = 0;
-    while (++i < argc)
-    {
-        ft_bzero(&file, sizeof(file));
-        if ((ret = load_file(argv[i], &file) == SUCCESS))
-        {
-            otool(&file);
-            free(file.name);
-            munmap(file.memory, file.size);
-        }
-    }
-    return (SUCCESS);
+	(void)argc;
+	if (!config_nm_otool(&nm_otool, argv, env, OTOOL_OPTIONS))
+		return (EXIT_FAILURE);
+	return (list_objs_symbols(&nm_otool, &obj_handler));
 }
---
+++ otool/srcs/main/otool_obj_handler.c
@@ -0,0 +1,24 @@
+#include "nm_otool.h"
+#include "otool.h"
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+int					otool_obj_handler(t_file *file)
+{
+    uint32_t    magic;
+
+    magic = *(uint32_t *)file->memory;
+    if (magic == MH_MAGIC_64 || magic == MH_CIGAM_64)
+        macho_64(file);
+    else if (magic == MH_MAGIC || magic == MH_CIGAM)
+        macho_32(file);
+    else if (magic == FAT_MAGIC || magic == FAT_CIGAM)
+        fat(file);
+    else if (ft_strncmp((char *)file->memory, ARMAG, SARMAG) == 0)
+    {
+        ft_printf("Archive : %s\n", file->name);
+        file->format = ARCHIVE;
+        archive(file);
+    }
+    return (SUCCESS);
+}
