ab5788895fbbe5157ac92c77372174551d747732
Federico <fzfloris@gmail.com>
2018-12-06 11:24:33 +0100

    Delete load_file.c

--- otool/includes/otool.h
+++ otool/includes/otool.h
@@ -1,41 +1,30 @@
 #ifndef OTOOL_H
 # define OTOOL_H
 
-# include <mach-o/fat.h>
-# include "libft.h"
-
 # define OTOOL_OPTIONS "t"
 
-# define SUCCESS 1
-# define FAILURE 0
+# define FAT 10
 # define WORD_NUM 16
-# define ARCHIVE 'a'
-# define FAT 'f'
 
 /*
 ** archive.c
 */
-int             archive(t_file *file);
+bool      archive(t_file *file);
 
 /*
 ** fat_64.c
 */
-int             fat(t_file *file);
-
-/*
-** load_file.c
-*/
-int             load_file(char *filename, t_file *file);
+bool      fat(t_file *file);
 
 /*
 ** macho_32.c
 */
-int             macho_32(t_file *file);
+bool      macho_32(t_file *file);
 
 /*
 ** macho_64.c
 */
-int             macho_64(t_file *file);
+bool      macho_64(t_file *file);
 
 /*
 **  otool_obj_handler.c
@@ -45,11 +34,11 @@ bool			otool_obj_handler(t_nm_otool *nm_otool);
 /*
 ** print.c
 */
-void            print_filename(t_file *file, char *current_file);
+void      print_filename(t_file *file, char *current_file);
 
 /*
 ** swap.c
 */
-uint32_t    swap_endian(uint32_t value);
+uint32_t  swap_endian(uint32_t value);
 
 #endif
