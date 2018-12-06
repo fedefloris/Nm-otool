1f99156b71f281e91821cbb1b2e048b652e56461
Dav HOJT <davhojt@gmail.com>
2018-12-06 17:44:40 +0100

    Created swap function.

--- making_the_best_ever_universal_endian_reverser.c
+++ making_the_best_ever_universal_endian_reverser.c
@@ -1,4 +1,19 @@
 #include <mach-o/fat.h>
+#include <stdio.h>
+
+void				swap_endian(unsigned char *value, int size)
+{
+	int				i;
+	unsigned char	tmp;
+	i = 0;
+	while (i < --size)
+	{
+		tmp = value[i];
+		value[i] = value[size];
+		value[size] = tmp;
+		i++;
+	}
+}
 
 int					main(void)
 {
@@ -7,5 +22,11 @@ int					main(void)
 	uint32_t		x32 = 0xff884400;
 	uint64_t		x64 = 0xffaa997755331100;
 
+	printf("%2x   %4x   %8x   %16llx\n", x8, x16, x32, x64);
+	swap_endian((unsigned char *)&x8, sizeof(x8));
+	swap_endian((unsigned char *)&x16, sizeof(x16));
+	swap_endian((unsigned char *)&x32, sizeof(x32));
+	swap_endian((unsigned char *)&x64, sizeof(x64));
+	printf("%2x   %4x   %8x   %16llx\n", x8, x16, x32, x64);
 	return (0);
 }
