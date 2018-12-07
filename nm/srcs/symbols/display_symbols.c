9744940eb7ba8850e31dd1949f1d098bf020ce7a
Federico <fzfloris@gmail.com>
2018-12-07 15:08:06 +0100

    Call sort_symbols before display_symbols

--- nm/srcs/symbols/display_symbols.c
+++ nm/srcs/symbols/display_symbols.c
@@ -1,17 +1,13 @@
 #include "nm_otool.h"
 #include "nm.h"
 
-void					display_symbols(t_nm_otool *nm_otool, t_symbol **symbols)
+void					display_symbols(t_nm_otool *nm_otool, t_symbol *symbols)
 {
-	t_symbol		*sym;
-
 	if (nm_otool->print_file_name)
 		ft_printf("\n%s:\n", nm_otool->file.name);
-	sort_symbols(nm_otool, symbols);
-	sym = *symbols;
-	while (sym)
+	while (symbols)
 	{
-		display_symbol(nm_otool, sym);
-		sym = sym->next;
+		display_symbol(nm_otool, symbols);
+		symbols = symbols->next;
 	}
 }
