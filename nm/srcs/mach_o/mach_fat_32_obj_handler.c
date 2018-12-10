a1e34eea7fe3aaa998f8aa32e1786055f304f035
Dav Højt <davhojt@gmail.com>
2018-12-10 06:44:24 +0100

    now uses MACRO SWAP_ENDIAN_FORCE

--- nm/srcs/mach_o/mach_fat_32_obj_handler.c
+++ nm/srcs/mach_o/mach_fat_32_obj_handler.c
@@ -1,40 +1,6 @@
 #include "nm_otool.h"
 #include "nm.h"
 
-void	 swap_64(unsigned char word)
-{
-	unsigned char	tmp;
-	unsigned char	new_word;
-	int				i;
-
-	i = 0;
-	while (i < 4)
-	{
-		tmp = word & 3;
-		new_word = new_word << 2;
-		new_word |= tmp;
-		word = word >> 2;
-		i++;
-	}
-	//ft_printf("------->value = %.2x\n", y);
-}
-
-uint32_t    swap_endian(uint32_t value)
-{
-	uint32_t    result;
-	uint32_t    byte1;
-	uint32_t    byte2;
-	uint32_t    byte3;
-	uint32_t    byte4;
-
-	byte1 = (value & 0xFF) << 24;
-	byte2 = (value & 0xFF00) << 8;
-	byte3 = (value & 0xFF0000) >> 8;
-	byte4 = (value & 0xFF000000) >> 24;
-	result = byte1 | byte2 | byte3 | byte4;
-	return (result);
-}
-
 static bool			mach_fat_32_launch_mach_o(t_nm_otool *nm_otool, struct fat_arch *arch, uint32_t nfat_arch, bool (*mach_o_function)(t_nm_otool *))
 {
 	bool			status;
@@ -49,7 +15,7 @@ static bool			mach_fat_32_launch_mach_o(t_nm_otool *nm_otool, struct fat_arch *a
 		ft_bzero(&nm_otool->file, sizeof(nm_otool->file));
 		nm_otool->file.name = file_data.name;
 		nm_otool->file.size = arch->size;
-		nm_otool->file.memory = file_data.memory + swap_endian(arch->offset);
+		nm_otool->file.memory = file_data.memory + SWAP_ENDIAN_FORCE(arch->offset);
 		if ((nm_otool->file.end_of_file = file_data.memory + file_data.size - 1) > file_data.end_of_file)//Check if safe
 			return (ERROR_LOG("fat: arch->size bad size."));
 		nm_otool->file.endian_is_reversed = file_data.endian_is_reversed;
@@ -66,9 +32,9 @@ static bool			mach_fat_32_handle_format(t_nm_otool *nm_otool, struct fat_arch *a
 {
 	if (!STRUCT_IS_SAFE(arch))
 		return (ERROR_LOG("fat: arch beyond binary"));
-	if (SWAP_ENDIAN((arch->cputype)) == CPU_TYPE_X86_64)
+	if (SWAP_ENDIAN_FORCE((arch->cputype)) == CPU_TYPE_X86_64)
 		return (mach_fat_32_launch_mach_o(nm_otool, arch, nfat_arch, &mach_o_64_obj_handler));
-	else if (swap_endian(arch->cputype) == CPU_TYPE_I386)
+	else if (SWAP_ENDIAN_FORCE(arch->cputype) == CPU_TYPE_I386)
 		return (mach_fat_32_launch_mach_o(nm_otool, arch, nfat_arch, &mach_o_32_obj_handler));
 	return (ERROR_LOG("fat: bad arch->cputype"));
 }
@@ -83,7 +49,7 @@ bool				mach_fat_32_obj_handler(t_nm_otool *nm_otool)
 		return (ERROR_LOG("fat: header beyond binary"));
 	if (!STRUCT_IS_SAFE(header))
 		return (ERROR_LOG("fat: header beyond binary"));
-	nfat_arch = swap_endian(header->nfat_arch);
+	nfat_arch = SWAP_ENDIAN_FORCE(header->nfat_arch);
 	if (!SET(arch, header + sizeof(*header)))
 		return (ERROR_LOG("fat: did not find arch within binary"));
 	return (mach_fat_32_handle_format(nm_otool, arch, nfat_arch));
