26c1cdb6b3aa3814d983cf4ecbcdecd88d5f3d11
Dav Højt <davhojt@gmail.com>
2018-12-10 23:33:40 +0100

    refactored code.

--- nm/srcs/symbols/display_symbol.c
+++ nm/srcs/symbols/display_symbol.c
@@ -1,26 +1,37 @@
 #include "nm_otool.h"
 #include "nm.h"
 
-static void	  display_64_symbol_value(t_symbol *sym)
+static void			display_64_symbol_value(t_symbol *sym, bool print_value)
 {
-	if (sym->value &&
-			(sym->type != 'U' && sym->type != 'u' && sym->type != 'I'))
+	if (print_value)
 		ft_printf("%-17.16jx", sym->value);
 	else
 		ft_printf("%17s", "");
 }
 
-static void	  display_32_symbol_value(t_symbol *sym)
+static void			display_32_symbol_value(t_symbol *sym, bool print_value)
 {
-	if (sym->value &&
-			(sym->type != 'U' && sym->type != 'u' && sym->type != 'I'))
+	if (print_value)
 		ft_printf("%-9.8jx", sym->value);
 	else
 		ft_printf("%9s", "");
 }
 
-void		      display_symbol(t_nm_otool *nm_otool, t_symbol *sym)
+static bool			should_the_value_be_printed(t_symbol *sym)
 {
+	if (sym->value
+			&& sym->type != 'U'
+			&& sym->type != 'u'
+			&& sym->type != 'I')
+		return (true);
+	return (false);
+}
+
+void				display_symbol(t_nm_otool *nm_otool, t_symbol *sym)
+{
+	bool			print_value;
+
+	print_value = should_the_value_be_printed(sym);
 	if (!sym->name || !*sym->name)
 		return ;
 	if (op(nm_otool, 'j'))
@@ -30,9 +41,9 @@ void		      display_symbol(t_nm_otool *nm_otool, t_symbol *sym)
 	}
 	if (nm_otool->file.format == MACH_O_32
 			|| nm_otool->file.format == ELF_32)
-		display_32_symbol_value(sym);
+		display_32_symbol_value(sym, print_value);
 	else
-		display_64_symbol_value(sym);
+		display_64_symbol_value(sym, print_value);
 	if (sym->type != 'I')
 		ft_printf("%c %s\n", sym->type, sym->name);
 	else
