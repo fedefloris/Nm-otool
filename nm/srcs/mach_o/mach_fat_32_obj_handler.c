7a7551f2ac44c82dce2b0512701e9b54ce2ff441
Dav Højt <davhojt@gmail.com>
2018-12-14 05:06:18 +0100

    rrefactored kaunch_mach_o in fat.

--- nm/srcs/mach_o/mach_fat_32_obj_handler.c
+++ nm/srcs/mach_o/mach_fat_32_obj_handler.c
@@ -3,18 +3,16 @@
 
 static bool			mach_fat_32_launch_mach_o(t_nm_otool *nm_otool, t_file file_data, struct fat_arch *arch, bool (*mach_o_function)(t_nm_otool *))
 {
-		if (!STRUCT_IS_SAFE(arch))
-			return (ERROR_LOG("fat: arch beyond binary"));
-		ft_bzero(&nm_otool->file, sizeof(nm_otool->file));
-		nm_otool->file.name = file_data.name;
-		nm_otool->file.size = SWAP_ENDIAN_FORCE(arch->size);
-		nm_otool->file.memory = file_data.memory + SWAP_ENDIAN_FORCE(arch->offset);
-		if ((nm_otool->file.end_of_file = file_data.memory + file_data.size - 1) > file_data.end_of_file)//Check if safe
-			return (ERROR_LOG("fat: arch->size bad size."));
-		nm_otool->file.endian_is_reversed = file_data.endian_is_reversed;
-		if (mach_o_function(nm_otool))
-			return (true);
-	return (false);
+	if (!STRUCT_IS_SAFE(arch))
+		return (ERROR_LOG("fat: arch beyond binary"));
+	ft_bzero(&nm_otool->file, sizeof(nm_otool->file));
+	nm_otool->file.name = file_data.name;
+	nm_otool->file.size = SWAP_ENDIAN_FORCE(arch->size);
+	nm_otool->file.memory = file_data.memory + SWAP_ENDIAN_FORCE(arch->offset);
+	if ((nm_otool->file.end_of_file = file_data.memory + file_data.size - 1) > file_data.end_of_file)//Check if safe
+		return (ERROR_LOG("fat: arch->size bad size."));
+	nm_otool->file.endian_is_reversed = file_data.endian_is_reversed;
+	return (mach_o_function(nm_otool));
 }
 
 static bool			mach_fat_32_handle_format(t_nm_otool *nm_otool, struct fat_arch *arch, uint32_t nfat_arch)
