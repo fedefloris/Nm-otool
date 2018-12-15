9f990ea53db38a37087cdc54ec466da36d6a77b3
Dav Højt <davhojt@gmail.com>
2018-12-15 20:59:57 +0100

    Temp fix for Endian issue.

--- nm/srcs/mach_o/mach_o_fat_32.c
+++ nm/srcs/mach_o/mach_o_fat_32.c
@@ -13,7 +13,7 @@ static bool		mach_fat_32_launch_mach_o(t_nm_otool *nm_otool,
 	nm_otool->file.memory = file_data.memory + SWAP_ENDIAN_FORCE(arch->offset);
 	if ((nm_otool->file.end_of_file = nm_otool->file.memory + nm_otool->file.size - 1) > file_data.end_of_file)
 		return (ERROR_LOG("fat: arch->size bad size."));
-	nm_otool->file.endian_is_reversed = file_data.endian_is_reversed;
+	//nm_otool->file.endian_is_reversed = file_data.endian_is_reversed;
 	return (mach_o_function(nm_otool));
 }
 
