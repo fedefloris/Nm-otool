f9ed93a3a6b9ac11d1c1bf25cb5b312e5c4e0d42
Dav Højt <davhojt@gmail.com>
2018-12-16 02:00:25 +0100

    Fix - now returns 'S' type

--- nm/srcs/mach_o/mach_o_get_type.c
+++ nm/srcs/mach_o/mach_o_get_type.c
@@ -1,16 +1,16 @@
 #include "nm_otool.h"
 #include "nm.h"
 
-static char			get_type_section(char type,
-		uint8_t n_sect, uint8_t **sections)
+static char			get_type_section( uint8_t n_sect, uint8_t **sections)
 {
+	//ft_printf("[%s] Looking for %d\n", (char *)sections[n_sect], n_sect);
 	if (sections[n_sect] && !ft_strcmp((char *)sections[n_sect], SECT_BSS))
 		return ('B');
 	if (sections[n_sect] && !ft_strcmp((char *)sections[n_sect], SECT_DATA))
 		return ('D');
 	if (sections[n_sect] && !ft_strcmp((char *)sections[n_sect], SECT_TEXT))
 		return ('T');
-	return (type);
+	return ('S');
 }
 
 char				mach_o_get_type(uint8_t n_type, uint64_t n_value,
@@ -24,7 +24,7 @@ char				mach_o_get_type(uint8_t n_type, uint64_t n_value,
 	type = ((n_type & N_TYPE) == N_ABS) ? 'A' : type;
 	type = ((n_type & N_TYPE) == N_PBUD) ? 'U' : type;
 	if ((n_type & N_TYPE) == N_SECT)
-		type = get_type_section(type, n_sect, sections);
+		type = get_type_section(n_sect, sections);
 	type = ((n_type & N_TYPE) == N_INDR) ? 'I' : type;
 	type = ((n_type & N_STAB) != 0) ? '-' : type;
 	type += ((n_type & N_EXT) == 0 && type != '0' && type != '-') ? 32 : 0;
