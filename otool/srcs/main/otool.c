3c92b86bec0c2a26872a90ca0a8d1c211da9fbaf
Dav Højt <davhojt@gmail.com>
2018-12-19 20:59:17 +0100

    Modified how options is string is passed to options.

--- otool/srcs/main/otool.c
+++ otool/srcs/main/otool.c
@@ -6,7 +6,7 @@ int					main(int argc, char **argv, char **env)
 	t_nm_otool	nm_otool;
 
 	(void)argc;
-	if (!config_nm_otool(&nm_otool, argv, env, OTOOL_OPTIONS))
+	if (!config_nm_otool(&nm_otool, argv, env, FT_OTOOL))
 		return (EXIT_FAILURE);
 	return (list_objs_symbols(&nm_otool, &otool_obj_handler));
 }
--- shared/srcs/config_nm_otool.c
+++ shared/srcs/config_nm_otool.c
@@ -1,13 +1,14 @@
 #include "nm_otool.h"
 
 bool				config_nm_otool(t_nm_otool *nm_otool, char **argv,
-	char **env, char *valid_options)
+	char **env, unsigned char routine)
 {
 	ft_bzero(nm_otool, sizeof(*nm_otool));
-	if (!options(&argv, valid_options, &nm_otool->options))
+	if (!options(&argv, (routine == FT_NM)? NM_OPTIONS : OTOOL_OPTIONS, &nm_otool->options))
 		return (false);
 	nm_otool->argv = argv;
 	nm_otool->env = env;
 	nm_otool->print_file_name = (*argv && *(argv + 1)) ? true : false;
+	nm_otool->routine = FT_OTOOL;
 	return (true);
 }
