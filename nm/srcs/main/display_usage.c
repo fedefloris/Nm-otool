9f0ad474fa75b4435b44ac71a2aa938d796fdef3
fedefloris <fzfloris@gmail.com>
2018-12-21 13:40:06 +0100

    Improve display_usage output

--- nm/srcs/main/display_usage.c
+++ nm/srcs/main/display_usage.c
@@ -1,8 +1,18 @@
 #include "nm_otool.h"
 #include "nm.h"
 
-int		display_usage(void)
+static void		display_options(void)
 {
-	ft_printf("Usage: nm [options] [files]\n");
+	ft_putstr("\t-j\tDisplay only symbols values\n");
+	ft_putstr("\t-p\tDo not sort the symbols\n");
+	ft_putstr("\t-r\tReverse the sense of the sort\n");
+}
+
+int				display_usage(void)
+{
+	ft_putstr("Usage: nm [options] [files]\n\n");
+	ft_putstr("List object files symbols.\n\n");
+	ft_putstr("The options are:\n");
+	display_options();
 	return (EXIT_SUCCESS);
 }
