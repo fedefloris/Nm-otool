9caba47b609b69a8adc5e20836d730e437cc8114
fedefloris <fzfloris@gmail.com>
2018-12-21 17:33:42 +0100

    Remove notes

--- nm/srcs/elf/elf_obj_handler_32.c
+++ nm/srcs/elf/elf_obj_handler_32.c
@@ -1,15 +1,6 @@
 #include "nm_otool.h"
 #include "nm.h"
 
-// Check if e_shnum is larger than or equal to SHN_LORESERVE (man elf)
-// should I use e_shentsize?
-// Check if e_shstrndx is larger than or equal to SHN_LORESERVE (man elf)
-// Some  section header table indices are reserved: the initial entry and the indices between
-// SHN_LORESERVE and SHN_HIRESERVE.  The initial entry is used  in  ELF  exten‐
-// sions  for  e_phnum, e_shnum and e_strndx; in other cases, each field in the
-// initial entry is set to zero.  An object file does  not  have  sections  for
-// these special indices
-
 bool		elf_obj_handler_32(t_nm_otool *nm_otool)
 {
 	Elf32_Ehdr	*header;
