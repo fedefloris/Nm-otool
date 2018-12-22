20a3be5bfa95e919d0877b240bd38be44cb5b6a1
fedefloris <fzfloris@gmail.com>
2018-12-22 08:41:18 +0100

    Code refactoring

--- otool/srcs/main/obj_handler.c
+++ otool/srcs/main/obj_handler.c
@@ -3,21 +3,17 @@
 
 bool            obj_handler(t_nm_otool *nm_otool)
 {
-    uint32_t    magic;
-
-    magic = *(uint32_t *)nm_otool->file.memory;
-    if (magic == MH_MAGIC_64 || magic == MH_CIGAM_64)
+    if (IS_MACH_O_64(nm_otool->file.format))
         mach_o_obj_handler_64(nm_otool);
-    else if (magic == MH_MAGIC || magic == MH_CIGAM)
+    else if (IS_MACH_O_32(nm_otool->file.format))
         mach_o_obj_handler_32(nm_otool);
-    else if (magic == FAT_MAGIC || magic == FAT_CIGAM)
+    else if (IS_MACH_O_FAT(nm_otool->file.format))
         mach_o_fat_32(nm_otool);
-    else if (ft_strncmp((char *)nm_otool->file.memory, ARMAG, SARMAG) == 0)
+    else if (IS_ARCHIVE(nm_otool->file.format))
     {
         ft_printf("Archive : %s\n", nm_otool->file.name);
-        nm_otool->file.format = ARCHIVE;
+        nm_otool->file.format = ARCHIVE; // Useless?
         archive_handler(nm_otool);
-        
     }
     return (true);
 }
