63700ed4379164cfaf60d6681d0ce014a5fcf78f
Dav Højt <davhojt@gmail.com>
2018-12-25 18:42:30 +0200

    refactored print row

--- otool/srcs/main/print_row.c
+++ otool/srcs/main/print_row.c
@@ -62,29 +62,27 @@ void		print_row(unsigned char *byte)
 {
 	unsigned char	row[(BYTES_PER_ROW * 3) + 1];
 	char			*number;
-	int				current_byte;
+	int				row_index;
 	int				position_on_row;
 
-	current_byte = 0;
+	row_index = 0;
 	position_on_row = 0;
 	number = NULL;
 	while (position_on_row < BYTES_PER_ROW)
 	{
-		if ((number = ft_itoa_base_tmp(*byte, 16, 'a')))//Get rid of this and use with your own ft_itoa_base (the one in our dev libft does not work)
+		if ((number = ft_itoa_base_tmp(byte[position_on_row], 16, 'a')))//Get rid of this and use with your own ft_itoa_base (the one in our dev libft does not work)
 		{
 			if (ft_strlen(number) == 1)
-				row[current_byte++] = '0';
-			ft_strcpy((char *)(row + current_byte), number);
-			current_byte += ft_strlen(number);
+				row[row_index++] = '0';
+			ft_strcpy((char *)(row + row_index), number);
+			row_index += ft_strlen(number);
 		}
 		else
-			ft_strcpy((char *)(row + current_byte), "00");
-
-		row[current_byte++] = ' ';
+			ft_strcpy((char *)(row + row_index), "00");
+		row[row_index++] = ' ';
 		position_on_row++;
-		byte++;
 		ft_strdel(&number);
 	}
-	row[current_byte] = '\0';
+	row[row_index] = '\0';
 	ft_printf("%s", row);
 }
