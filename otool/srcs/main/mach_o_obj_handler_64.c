09641c8d67b3d8a890617336bdeb0a945a9759e4
Dav Højt <davhojt@gmail.com>
2018-12-25 16:31:19 +0200

    parse_text_64 is now almost reuseable for parse_text_32 except for prototype.

--- otool/srcs/main/mach_o_obj_handler_64.c
+++ otool/srcs/main/mach_o_obj_handler_64.c
@@ -4,6 +4,7 @@
 bool		parse_text_64(struct section_64 *section, t_nm_otool *nm_otool)
 {
 	uint64_t			addr;
+	uint64_t			size;
 	uint32_t			current_byte;
 	uint32_t			position_on_row;
 	unsigned char		*byte;
@@ -13,19 +14,21 @@ bool		parse_text_64(struct section_64 *section, t_nm_otool *nm_otool)
 	if (!SET(byte, nm_otool->file.memory + section->offset))
 		return (ERROR_LOG("offset beyond binary"));
 	addr = section->addr;
+	size = section->size;
 	current_byte = 0;
-	while (current_byte < section->size)
+	while (current_byte < size)
 	{
 		ft_printf("%016lx\t", addr + current_byte);
 		position_on_row = 0;
-		while (position_on_row < BYTES_PER_ROW && (current_byte + position_on_row) < section->size)
+		while (position_on_row < BYTES_PER_ROW && current_byte < size)
 		{
-			if (current_byte + position_on_row + BYTES_PER_ROW < section->size)
+			if (current_byte + position_on_row + BYTES_PER_ROW < size)
 			{
 				print_row(byte);
 				if (!SET(byte, byte + BYTES_PER_ROW))
 					return (ERROR_LOG("byte + BYTES_PER_ROW is beyond binary"));
 				position_on_row += BYTES_PER_ROW;
+				current_byte += BYTES_PER_ROW;
 			}
 			else
 			{
@@ -33,10 +36,10 @@ bool		parse_text_64(struct section_64 *section, t_nm_otool *nm_otool)
 				if (!SET(byte, byte + sizeof(*byte)))
 					return (ERROR_LOG("byte + sizeof(byte) is beyond binary"));
 				position_on_row++;
+				current_byte++;
 			}
 		}
 		ft_printf("\n");
-		current_byte += BYTES_PER_ROW;
 	}
 	return (true);
 }
