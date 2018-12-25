d1c179ade78720a5a0730ee5904c22c52aeef569
Dav Højt <davhojt@gmail.com>
2018-12-25 18:49:51 +0200

    Made print_byte into own file

--- otool/srcs/main/mach_o_obj_handler_64.c
+++ otool/srcs/main/mach_o_obj_handler_64.c
@@ -1,18 +1,6 @@
 #include "nm_otool.h"
 #include "otool.h"
 
-static bool			display_byte(t_nm_otool *nm_otool, unsigned char **byte, uint64_t *current_byte, uint64_t *position_on_row)
-{
-	if (!ADDRESS_IS_SAFE(*byte))
-		return (ERROR_LOG("current row is beyond binary"));
-	ft_printf("%02x ", **byte);
-	if (!ADVANCE(*byte, *byte + sizeof(**byte)))
-		return (ERROR_LOG("next byte is beyond binary"));
-	*position_on_row += 1;
-	*current_byte += 1;
-	return (true);
-}
-
 static bool			parse_text_64(t_nm_otool *nm_otool,
 	struct section_64 *section)
 {
