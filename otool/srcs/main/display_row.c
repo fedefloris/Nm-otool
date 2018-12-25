4290fe69e1dd609c3d72e204676fb87799b3fcbd
Dav Højt <davhojt@gmail.com>
2018-12-25 18:47:16 +0200

    Made display_row into own file

--- otool/srcs/main/display_row.c
+++ otool/srcs/main/display_row.c
@@ -58,7 +58,7 @@ static char	*ft_itoa_base_tmp(uintmax_t num, uintmax_t base, char c)
 	return (str);
 }
 
-void		print_row(unsigned char *byte)
+static void			print_row(unsigned char *byte)
 {
 	unsigned char	row[(BYTES_PER_ROW * 3) + 1];
 	char			*number;
@@ -85,4 +85,17 @@ void		print_row(unsigned char *byte)
 	}
 	row[row_index] = '\0';
 	ft_printf("%s", row);
+}
+
+bool				display_row(t_nm_otool *nm_otool, unsigned char **byte,
+				uint64_t *current_byte, uint64_t *position_on_row)
+{
+	if (!ADDRESS_IS_SAFE(*byte + (BYTES_PER_ROW - 1)))
+		return (ERROR_LOG("current row is beyond binary"));
+	print_row(*byte);
+	if (!ADVANCE(*byte, *byte + BYTES_PER_ROW))
+		return (ERROR_LOG("next row is beyond binary"));
+	*position_on_row += BYTES_PER_ROW;
+	*current_byte += BYTES_PER_ROW;
+	return (true);
 }
