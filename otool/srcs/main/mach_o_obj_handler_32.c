8ea2ea824d02b84906213d5a7c4398cf289bb59c
Dav Højt <davhojt@gmail.com>
2018-12-25 19:19:15 +0200

    Created 32 - mirroring 64 of otools mach-o_handler

--- otool/srcs/main/mach_o_obj_handler_32.c
+++ otool/srcs/main/mach_o_obj_handler_32.c
@@ -1,76 +1,88 @@
 #include "nm_otool.h"
 #include "otool.h"
 
-bool		parse_text(struct section *section, t_nm_otool *nm_otool)
+static bool			parse_text_32(t_nm_otool *nm_otool,
+	struct section *section)
 {
-	struct section		*sect_ptr;
-	struct section		*sect_addr;
-	uint32_t			i;
-	uint32_t			j;
-	unsigned char		byte;
+	bool			status;
+	uint64_t		current_byte;
+	uint64_t		position_on_row;
+	unsigned char	*byte;
 
-	sect_ptr = (void *)nm_otool->file.memory + section->offset;
-	sect_addr = section;
-	i = 0;
-	while (i < section->size)
+	if (!STRUCT_IS_SAFE(section))
+		return (ERROR_LOG("section is beyond binary"));
+	if (!SET(byte, nm_otool->file.memory + SWAP_ENDIAN(section->offset)))
+		return (ERROR_LOG("offset beyond binary"));
+	current_byte = 0;
+	status = true;
+	while (current_byte < SWAP_ENDIAN(section->size))
 	{
-		ft_printf("%08lx\t", (void *)(size_t)(sect_addr->addr + i));
-		j = 0;
-		while (j < BYTES_PER_ROW && (i + j) < section->size)
+		ft_printf("%08lx\t", SWAP_ENDIAN(section->addr) + current_byte);
+		position_on_row = 0;
+		while (position_on_row < BYTES_PER_ROW && current_byte < SWAP_ENDIAN(section->size))
 		{
-			byte = *(unsigned char *)sect_ptr;
-			ft_printf("%02x ", byte);
-			//swap_64(byte);
-			sect_ptr = (void *)sect_ptr + sizeof(char);
-			j++;
+			if (current_byte + position_on_row + BYTES_PER_ROW < SWAP_ENDIAN(section->size))
+				status = display_row(nm_otool, &byte, &current_byte, &position_on_row);
+			else
+				status = display_byte(nm_otool, &byte, &current_byte, &position_on_row);
+			if (!status)
+				return (false);
 		}
 		ft_printf("\n");
-		i += BYTES_PER_ROW;
 	}
 	return (true);
 }
 
-bool		text_segment(struct load_command *lcmd, t_nm_otool *nm_otool)
+static bool			text_segment_32(t_nm_otool *nm_otool,
+	struct segment_command *segment)
 {
-	struct segment_command		*segment;
-	struct section  			*section;
-	void						*sect_ptr;
-	uint32_t					i;
+	struct section		*section;
+	uint32_t			nsects;
 
-	segment = (struct segment_command *)lcmd;
-	sect_ptr = (void *)(segment + 1);
-	i = 0;
-	while (i < segment->nsects)
+	if (!SET(section, segment + sizeof(*segment)))
+		return (ERROR_LOG("first section beyond binary"));
+	if (!STRUCT_IS_SAFE(segment))
+		return (ERROR_LOG("segment struct is beyond binary"));
+	nsects = SWAP_ENDIAN(segment->nsects);
+	while (nsects--)
 	{
-		section = (struct section *)sect_ptr;
-		if (ft_strcmp(section->sectname, SECT_TEXT) == 0)
+		if (!STRUCT_IS_SAFE(section) || !STRING_IS_SAFE(section->sectname))
+			return (ERROR_LOG("section or secname is beyond binary"));
+		if (!ft_strcmp(section->sectname, SECT_TEXT))
 		{
 			ft_printf("Contents of (__TEXT,__text) section\n");
-			parse_text(section, nm_otool);
+			return (parse_text_32(nm_otool, section));
 		}
-		sect_ptr += sizeof(struct section);
-		i++;
+		if (!SET(section, section + sizeof(*section)))
+			return (ERROR_LOG("next section is beyond binary"));
 	}
 	return (true);
 }
 
-bool		mach_o_obj_handler_32(t_nm_otool *nm_otool)
+bool				mach_o_obj_handler_32(t_nm_otool *nm_otool)
 {
 	struct mach_header		*header;
 	struct load_command		*lcmd;
-	uint32_t				i;
+	uint32_t				ncmds;
 
 	if (nm_otool->print_file_name)
 		ft_printf("%s:\n", nm_otool->file.name);
-	header = (struct mach_header *)nm_otool->file.memory;
-	lcmd = (void *)nm_otool->file.memory + sizeof(*header);
-	i = 0;
-	while (i < header->ncmds)
+	if (!SET(header, nm_otool->file.memory))
+		return (ERROR_LOG("file->memory beyond binary"));
+	if (!SET(lcmd, nm_otool->file.memory + sizeof(*header)))
+		return (ERROR_LOG("first load command beyond binary"));
+	if (!STRUCT_IS_SAFE(header))
+		return (ERROR_LOG("header is beyond binary"));
+	ncmds = SWAP_ENDIAN(header->ncmds);
+	while (ncmds--)
 	{
-		if (lcmd->cmd == LC_SEGMENT)
-			text_segment(lcmd, nm_otool);
-		lcmd = (void *)lcmd + lcmd->cmdsize;
-		i++;
+		if (!STRUCT_IS_SAFE(lcmd))
+			return (ERROR_LOG("current load command is beyond binary"));
+		if (SWAP_ENDIAN(lcmd->cmd) == LC_SEGMENT)
+			if (!text_segment_32(nm_otool, (struct segment_command *)lcmd))
+				return (false);
+		if (!ADVANCE(lcmd, lcmd + lcmd->cmdsize))
+			return (ERROR_LOG("next load command is beyond binary"));
 	}
 	return (true);
 }
