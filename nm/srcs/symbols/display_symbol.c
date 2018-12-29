4f91600ba643aff32217ee928d2c8b759337a684
fedefloris <fzfloris@gmail.com>
2018-12-29 11:50:50 +0100

    Start to use send_to_buffer inside nm

--- nm/srcs/symbols/display_symbol.c
+++ nm/srcs/symbols/display_symbol.c
@@ -1,20 +1,22 @@
 #include "nm_otool.h"
 #include "nm.h"
 
-static void			display_64_symbol_value(t_symbol *sym, bool print_value)
+static void			display_64_symbol_value(t_nm_otool *nm_otool,
+	t_symbol *sym, bool print_value)
 {
 	if (print_value)
 		ft_printf("%-17.16jx", sym->value);
 	else
-		ft_printf("%17s", "");
+		send_to_buffer(&nm_otool->buffer, "                 ", NULL);
 }
 
-static void			display_32_symbol_value(t_symbol *sym, bool print_value)
+static void			display_32_symbol_value(t_nm_otool *nm_otool,
+	t_symbol *sym, bool print_value)
 {
 	if (print_value)
 		ft_printf("%-9.8jx", sym->value);
 	else
-		ft_printf("%9s", "");
+		send_to_buffer(&nm_otool->buffer, "         ", NULL);
 }
 
 static bool			should_the_value_be_printed(t_symbol *sym)
@@ -30,19 +32,23 @@ static bool			should_the_value_be_printed(t_symbol *sym)
 void				display_symbol(t_nm_otool *nm_otool, t_symbol *sym)
 {
 	bool			print_value;
+	char			type[3];
 
-	print_value = should_the_value_be_printed(sym);
 	if (!sym->name || !*sym->name)
 		return ;
 	if (op(nm_otool, 'j'))
 	{
-		ft_printf("%s\n", sym->name);
+		send_to_buffer(&nm_otool->buffer, sym->name, "\n", NULL);
 		return ;
 	}
-	if (nm_otool->file.format == MACH_O_32
-			|| nm_otool->file.format == ELF_32)
-		display_32_symbol_value(sym, print_value);
+	type[0] = sym->type;
+	type[1] = ' ';
+	type[2] = '\0';
+	print_value = should_the_value_be_printed(sym);
+	if (IS_MACH_O_32(nm_otool->file.format)
+			|| IS_ELF_32(nm_otool->file.format))
+		display_32_symbol_value(nm_otool, sym, print_value);
 	else
-		display_64_symbol_value(sym, print_value);
-	ft_printf("%c %s\n", sym->type, sym->name);
+		display_64_symbol_value(nm_otool, sym, print_value);
+	send_to_buffer(&nm_otool->buffer, type, sym->name, "\n", NULL);
 }
--- nm/srcs/symbols/display_symbols.c
+++ nm/srcs/symbols/display_symbols.c
@@ -1,24 +1,14 @@
 #include "nm_otool.h"
 #include "nm.h"
 
-static void		send_file_name(t_buffer *buffer, char *file_name)
-{
-	send_to_buffer(&buffer, "\n");
-	send_to_buffer(&buffer, nm_otool->file.name);
-	send_to_buffer(&buffer, ":\n");
-}
-
 void			display_symbols(t_nm_otool *nm_otool, t_symbol *symbols)
 {
-	t_buffer buffer;
-
-	config_buffer(&buffer);
 	if (nm_otool->print_file_name)
-		send_file_name(&buffer, nm_otool->file.name);
+		send_to_buffer(&nm_otool->buffer, "\n", nm_otool->file.name, ":\n", NULL);
 	while (symbols)
 	{
 		display_symbol(nm_otool, symbols);
 		symbols = symbols->next;
 	}
-	empty_the_buffer(&buffer);
+	empty_the_buffer(&nm_otool->buffer);
 }
