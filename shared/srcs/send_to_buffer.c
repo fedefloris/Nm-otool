ace6bd429404b5e95b1fe3394c9ee586e18f6079
fedefloris <fzfloris@gmail.com>
2018-12-29 11:50:23 +0100

    Make send_to_buffer a varadic function

--- shared/srcs/send_to_buffer.c
+++ shared/srcs/send_to_buffer.c
@@ -1,6 +1,7 @@
 #include "nm_otool.h"
+#include <stdarg.h>
 
-void		send_to_buffer(t_buffer *buffer, char *str)
+void		send_str_to_buffer(t_buffer *buffer, char *str)
 {
 	while (*str)
 	{
@@ -8,4 +9,15 @@ void		send_to_buffer(t_buffer *buffer, char *str)
 			empty_the_buffer(buffer);
 		buffer->content[buffer->content_index++] = *str++;
 	}
+}
+
+void		send_to_buffer(t_buffer *buffer, ...)
+{
+	va_list		args;
+	char		*str;
+
+	va_start(args, buffer);
+	while ((str = va_arg(args, char*)))
+		send_str_to_buffer(str);
+	va_end(args);
 }
