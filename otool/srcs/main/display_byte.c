412f6801bb6324628998dccdf1b2239b6b9355df
fedefloris <fzfloris@gmail.com>
2018-12-30 18:34:06 +0100

    Use SEND_TO_BUFFER in display_byte.c

--- otool/srcs/main/display_byte.c
+++ otool/srcs/main/display_byte.c
@@ -1,12 +1,88 @@
 #include "nm_otool.h"
 #include "otool.h"
 
+static char	ft_calculate_char(int mod, char c)
+{
+	char	return_char;
+
+	if (mod > 36)
+		return (0);
+	return_char = '0';
+	while (mod--)
+	{
+		return_char++;
+		if (return_char == ':')
+			return_char = c;
+	}
+	return (return_char);
+}
+
+static int	ft_get_len(uintmax_t num, uintmax_t base)
+{
+	int		len;
+
+	len = 0;
+	while (num)
+	{
+		num /= base;
+		len++;
+	}
+	return (len);
+}
+
+//Get rid of this and use with your own ft_itoa_base (the one in our dev libft does not work)
+static char	*ft_itoa_base_tmp(uintmax_t num, uintmax_t base, char c)
+{
+	uintmax_t	sum;
+	int			mod;
+	int			len;
+	char		*str;
+
+	if (num == 0)
+	{
+		if (!(str = ft_strnew(1)))
+			return (NULL);
+		*str = '0';
+		return (str);
+	}
+	sum = num;
+	len = ft_get_len(num, base);
+	if (!(str = ft_strnew(len)))
+		return (NULL);
+	while (sum)
+	{
+		mod = sum % base;
+		sum /= base;
+		str[(len--) - 1] = ft_calculate_char(mod, c);
+	}
+	return (str);
+}
+
+static char			*get_formatted_byte(int byte)
+{
+	static char		formatted[3];
+	char			*number;
+	int				formatted_index;
+	int				number_index;
+
+	ft_memset(formatted, '0', 2);
+	if (!(number = ft_itoa_base_tmp(byte, 16, 'a'))) // Get rid of this and use with your own ft_itoa_base (the one in our dev libft does not work)
+		return ("");
+	formatted_index = 1;
+	if ((number_index = ft_strlen(number) - 1) > formatted_index)
+		number_index = formatted_index;
+	while (number_index >= 0)
+		formatted[formatted_index--] = number[number_index--];
+	ft_strdel(&number);
+	return (formatted);
+}
+
 bool				display_byte(t_nm_otool *nm_otool, unsigned char **byte,
 				uint64_t *current_byte, uint64_t *position_on_row)
 {
 	if (!ADDRESS_IS_SAFE(*byte))
 		return (ERROR_LOG("current row is beyond binary"));
-	ft_printf("%02x ", **byte);
+	SEND_TO_BUFFER(get_formatted_byte(**byte), " ");
 	if (!ADVANCE(*byte, *byte + sizeof(**byte)))
 		return (ERROR_LOG("next byte is beyond binary"));
 	*position_on_row += 1;
