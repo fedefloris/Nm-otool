75f7412780c9338fb04a5072ccfbcb5eb78b9ea1
ffloris <fzfloris@gmail.com>
2019-03-20 16:46:33 +0100

    Return good exit status for otool

--- otool/srcs/main/mach_o_obj_handler.c
+++ otool/srcs/main/mach_o_obj_handler.c
@@ -16,15 +16,17 @@
 bool		mach_o_obj_handler(t_nm_otool *nm_otool)
 {
 	if (IS_MACH_O_64(nm_otool->file.format))
-		mach_o_obj_handler_64(nm_otool);
+		return (mach_o_obj_handler_64(nm_otool));
 	else if (IS_MACH_O_32(nm_otool->file.format))
-		mach_o_obj_handler_32(nm_otool);
-	else if (IS_MACH_O_FAT(nm_otool->file.format))
-		mach_o_fat_32(nm_otool);
+		return (mach_o_obj_handler_32(nm_otool));
+	if (IS_MACH_O_FAT_32(nm_otool->file.format))
+		return (mach_o_fat_32(nm_otool));
+	if (IS_MACH_O_FAT_64(nm_otool->file.format))
+		return (mach_o_fat_64(nm_otool));
 	else if (IS_ARCHIVE(nm_otool->file.format))
 	{
 		nm_otool->file.format = ARCHIVE;
-		archive_handler(nm_otool);
+		return (archive_handler(nm_otool));
 	}
-	return (true);
+	return (ERROR_LOG("Unrecognized format"));
 }
